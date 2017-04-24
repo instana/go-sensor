d41402f7cabb46f2edd63938dd0f7d7c460b8c55
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-04-24 18:25:14 +0200

    Add tests to validate span parent/child span relationships

--- span_test.go
+++ span_test.go
@@ -2,12 +2,13 @@ package instana_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/instana/golang-sensor"
 	bt "github.com/opentracing/basictracer-go"
-	//opentracing "github.com/opentracing/opentracing-go"
+	ot "github.com/opentracing/opentracing-go"
 )
 
 func TestBasicSpan(t *testing.T) {
@@ -25,9 +26,44 @@ func TestBasicSpan(t *testing.T) {
 	assert.NotNil(t, span.Context, "Context is nil!")
 	assert.NotNil(t, span.Duration, "Duration is nil!")
 	assert.NotNil(t, span.Operation, "Operation is nil!")
-	assert.NotNil(t, span.ParentSpanID, "ParentSpan is nil!")
+	assert.Equal(t, span.ParentSpanID, uint64(0), "ParentSpan shouldn't have a value")
 	assert.NotNil(t, span.Start, "Start is nil!")
 	assert.Nil(t, span.Tags, "Tags is nil!")
+}
+
+func TestSpanHeritage(t *testing.T) {
+	recorder := bt.NewInMemoryRecorder()
+	tracer := instana.NewTracerWithEverything(&instana.Options{}, recorder)
+
+	parentSpan := tracer.StartSpan("parent")
+
+	childSpan := tracer.StartSpan("child", ot.ChildOf(parentSpan.Context()))
+	time.Sleep(2 * time.Millisecond)
+	childSpan.Finish()
+
+	time.Sleep(2 * time.Millisecond)
+	parentSpan.Finish()
+
+	spans := recorder.GetSpans()
+	assert.Equal(t, len(spans), 2)
+	cSpan := spans[0]
+	pSpan := spans[1]
+
+	assert.Equal(t, "child", cSpan.Operation, "Child span name doesn't compute")
+	assert.Equal(t, "parent", pSpan.Operation, "Parent span name doesn't compute")
+
+	// Parent should not have a parent
+	assert.Equal(t, pSpan.ParentSpanID, uint64(0), "ParentSpanID shouldn't have a value")
+
+	// Child must have parent ID set as parent
+	assert.Equal(t, pSpan.Context.SpanID, cSpan.ParentSpanID, "parentID doesn't match")
+
+	// Must be root span
+	assert.Equal(t, pSpan.Context.TraceID, pSpan.Context.SpanID, "not a root span")
+
+	// Trace ID must be consistent across spans
+	assert.Equal(t, cSpan.Context.TraceID, pSpan.Context.TraceID, "trace IDs don't match")
+
 }
 
 func TestSpanBaggage(t *testing.T) {
