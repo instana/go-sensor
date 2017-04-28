041f2d1f132a3e63fac38d6f4830ddec4804edd9
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-04-28 18:04:10 +0200

    Test range of generated IDs.

--- util_internal_test.go
+++ util_internal_test.go
@@ -1,6 +1,10 @@
 package instana
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // Trace IDs (and Span IDs) are based on Java Signed Long datatype
 const MinUint64 = uint64(0)
@@ -8,5 +12,15 @@ const MaxUint64 = uint64(18446744073709551615)
 const MinInt64 = int64(-9223372036854775808)
 const MaxInt64 = int64(9223372036854775807)
 
-func TestIDGeneration(t *testing.T) {
+func TestGeneratedIDRange(t *testing.T) {
+
+	var count = 10000
+	var id = int64(0)
+	for index := 0; index < count; index++ {
+		id = randomID()
+
+		assert.True(t, id <= 9223372036854775807, "Generated ID is out of bounds (+)")
+		assert.True(t, id >= -9223372036854775808, "Generated ID is out of bounds (-)")
+
+	}
 }
