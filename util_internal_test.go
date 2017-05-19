26955ef34126a2498abc5330eaaa4febd4cd5b4a
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-05-19 14:20:31 +0200

    Add tests to validate that leading zeros are dropped from hex strings.

--- util_internal_test.go
+++ util_internal_test.go
@@ -44,6 +44,36 @@ func TestIDConversionBackForth(t *testing.T) {
 	assert.Equal(t, minHex, header, "Convert back into original is wrong")
 }
 
+func TestIDConversion(t *testing.T) {
+	// Place holders
+	var header string
+	var id int64
+
+	header, _ = ID2Header(-7815363404733516491)
+	assert.Equal(t, "938a406416457535", header, "ID2Header incorrect result.")
+	id, _ = Header2ID("938a406416457535")
+	assert.Equal(t, int64(-7815363404733516491), id, "Header2ID incorrect result")
+
+	header, _ = ID2Header(307170163380978816)
+	assert.Equal(t, "44349a2d9ec0480", header, "ID2Header incorrect result.")
+	id, _ = Header2ID("44349a2d9ec0480") // Without a leading zero
+	assert.Equal(t, int64(307170163380978816), id, "Header2ID incorrect result")
+	id, _ = Header2ID("044349a2d9ec0480") // Try with a leading zero
+	assert.Equal(t, int64(307170163380978816), id, "Header2ID incorrect result")
+
+	header, _ = ID2Header(2920004540187184976)
+	assert.Equal(t, "2885f0a890628f50", header, "ID2Header incorrect result.")
+	id, _ = Header2ID("2885f0a890628f50")
+	assert.Equal(t, int64(2920004540187184976), id, "Header2ID incorrect result")
+
+	header, _ = ID2Header(16)
+	assert.Equal(t, "10", header, "ID2Header should drop leading zeros")
+	id, _ = Header2ID("0000000000000010")
+	assert.Equal(t, int64(16), id, "Header2ID should stll work with leading zeros")
+	id, _ = Header2ID("10")
+	assert.Equal(t, int64(16), id, "Header2ID should convert <16 char strings")
+}
+
 func TestBogusValues(t *testing.T) {
 	var id int64
 
