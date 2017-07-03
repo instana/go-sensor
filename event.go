17d79ab5edece28790207b2db9a1f83ec3e6082b
Michael Ernst <michael.ernst@sixt.com>
2017-07-03 10:56:58 +0200

    solve issues 31 and 33
    -add severity type
    -add severity constants
    -add duration

--- event.go
+++ event.go
@@ -1,41 +1,59 @@
 package instana
 
+import (
+	"time"
+)
+
 type EventData struct {
-	Title    string `json:"title"`
-	Text     string `json:"text"`
-	Duration int    `json:"duration"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+	// Duration in milliseconds
+	Duration int `json:"duration"`
+	// Severity with value of -1, 5, 10 : see type severity
 	Severity int    `json:"severity"`
 	Plugin   string `json:"plugin,omitempty"`
 	ID       string `json:"id,omitempty"`
 	Host     string `json:"host,omitempty"`
 }
 
+type severity int
+
+//Severity values for events sent to the instana agent
+const (
+	SeverityChange   severity = -1
+	SeverityWarning  severity = 5
+	SeverityCritical severity = 10
+)
+
 const (
 	ServicePlugin = "com.instana.forge.connection.http.logical.LogicalWebApp"
 	ServiceHost   = ""
 )
 
-func SendDefaultServiceEvent(title string, text string, severity int) {
-	SendServiceEvent(sensor.serviceName, title, text, severity)
+//SendDefaultServiceEvent sends a default event which already contains the service and host
+func SendDefaultServiceEvent(title string, text string, sev severity, duration time.Duration) {
+	SendServiceEvent(sensor.serviceName, title, text, sev, duration)
 }
 
-func SendServiceEvent(service string, title string, text string, severity int) {
+func SendServiceEvent(service string, title string, text string, sev severity, duration time.Duration) {
 	sendEvent(&EventData{
 		Title:    title,
 		Text:     text,
-		Duration: 0,
-		Severity: severity,
+		Severity: int(sev),
 		Plugin:   ServicePlugin,
 		ID:       service,
-		Host:     ServiceHost})
+		Host:     ServiceHost,
+		Duration: int(duration * time.Millisecond),
+	})
 }
 
-func SendHostEvent(title string, text string, severity int) {
+func SendHostEvent(title string, text string, sev severity, duration time.Duration) {
 	sendEvent(&EventData{
 		Title:    title,
 		Text:     text,
-		Duration: 0,
-		Severity: severity})
+		Duration: int(duration * time.Millisecond),
+		Severity: int(sev),
+	})
 }
 
 func sendEvent(event *EventData) {
