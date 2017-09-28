40152ca1f3eea4a7fc88f68c466b8b5d00891258
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-09-28 09:47:17 +0200

    Updated example; better naming and tracer level service naming

--- example/ot-simple/simple.go
+++ example/ot-simple/simple.go
@@ -11,33 +11,31 @@ import (
 )
 
 const (
-	Service = "simple-microservice"
+	// Service - use a tracer level global service name
+	Service = "Go-Overlord"
 )
 
 func simple(ctx context.Context) {
-	parentSpan, ctx := ot.StartSpanFromContext(ctx, "parent")
-	parentSpan.SetTag(string(ext.Component), Service)
+	parentSpan, ctx := ot.StartSpanFromContext(ctx, "asteroid")
+	parentSpan.SetTag(string(ext.Component), "Go simple example app")
 	parentSpan.SetTag(string(ext.SpanKind), string(ext.SpanKindRPCServerEnum))
-	parentSpan.SetTag(string(ext.PeerHostname), "localhost")
 	parentSpan.SetTag(string(ext.HTTPUrl), "/golang/simple/one")
 	parentSpan.SetTag(string(ext.HTTPMethod), "GET")
 	parentSpan.SetTag(string(ext.HTTPStatusCode), uint16(200))
-	parentSpan.LogFields(
-		log.String("foo", "bar"))
+	parentSpan.LogFields(log.String("foo", "bar"))
 
-	childSpan := ot.StartSpan("child", ot.ChildOf(parentSpan.Context()))
+	childSpan := ot.StartSpan("spacedust", ot.ChildOf(parentSpan.Context()))
 	childSpan.SetTag(string(ext.SpanKind), string(ext.SpanKindRPCClientEnum))
-	childSpan.SetTag(string(ext.PeerHostname), "localhost")
 	childSpan.SetTag(string(ext.HTTPUrl), "/golang/simple/two")
 	childSpan.SetTag(string(ext.HTTPMethod), "POST")
 	childSpan.SetTag(string(ext.HTTPStatusCode), 204)
 	childSpan.SetBaggageItem("someBaggage", "someValue")
 
-	time.Sleep(450 * time.Millisecond)
+	time.Sleep(10 * time.Millisecond)
 
 	childSpan.Finish()
 
-	time.Sleep(550 * time.Millisecond)
+	time.Sleep(5 * time.Millisecond)
 
 	parentSpan.Finish()
 }
@@ -45,9 +43,8 @@ func simple(ctx context.Context) {
 func main() {
 	ot.InitGlobalTracer(instana.NewTracerWithOptions(&instana.Options{
 		Service:  Service,
-		LogLevel: instana.Debug}))
+		LogLevel: instana.Info}))
 
-	go forever()
 	go forever()
 	select {}
 }
@@ -55,5 +52,6 @@ func main() {
 func forever() {
 	for {
 		simple(context.Background())
+		time.Sleep(2 * time.Second)
 	}
 }
