5eaf247b60cb99083b422b6e51d7868e287aaa90
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-02-15 15:11:45 +0100

    Update example: random durations and intermediate span

--- example/ot-simple/simple.go
+++ example/ot-simple/simple.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/instana/golang-sensor"
@@ -16,27 +17,38 @@ const (
 )
 
 func simple(ctx context.Context) {
-	parentSpan, ctx := ot.StartSpanFromContext(ctx, "asteroid")
+	// Handling an incoming request
+	parentSpan := ot.StartSpan("asteroid")
 	parentSpan.SetTag(string(ext.Component), "Go simple example app")
 	parentSpan.SetTag(string(ext.SpanKind), string(ext.SpanKindRPCServerEnum))
-	parentSpan.SetTag(string(ext.HTTPUrl), "/golang/simple/one")
+	parentSpan.SetTag(string(ext.HTTPUrl), "https://asteroid.svc.io/golang/api/v2")
 	parentSpan.SetTag(string(ext.HTTPMethod), "GET")
 	parentSpan.SetTag(string(ext.HTTPStatusCode), uint16(200))
 	parentSpan.LogFields(log.String("foo", "bar"))
 
+	// Making an HTTP request
 	childSpan := ot.StartSpan("spacedust", ot.ChildOf(parentSpan.Context()))
 	childSpan.SetTag(string(ext.SpanKind), string(ext.SpanKindRPCClientEnum))
-	childSpan.SetTag(string(ext.HTTPUrl), "/golang/simple/two")
+	childSpan.SetTag(string(ext.HTTPUrl), "https://meteor.svc.io/golang/api/v2")
 	childSpan.SetTag(string(ext.HTTPMethod), "POST")
 	childSpan.SetTag(string(ext.HTTPStatusCode), 204)
 	childSpan.SetBaggageItem("someBaggage", "someValue")
 
-	time.Sleep(10 * time.Millisecond)
-
+	// Make the HTTP request (we'll just sleep for a random duration as an example)
+	time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
 	childSpan.Finish()
 
-	time.Sleep(5 * time.Millisecond)
+	// Rendering a template with custom tags
+	renderSpan := ot.StartSpan("render", ot.ChildOf(childSpan.Context()))
+	renderSpan.SetTag("type", "layout")
+	renderSpan.SetTag("name", "application_layout.erb")
 
+	// Render the template (we'll just sleep for a random duration as an example)
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	renderSpan.Finish()
+
+	// Additional work (we'll just sleep for a random duration as an example)
+	time.Sleep(time.Duration(rand.Intn(30)) * time.Millisecond)
 	parentSpan.Finish()
 }
 
