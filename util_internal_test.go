daab5baa3eb5e3987c7b9507710e1c86360b2d85
Andrey Slotin <andrey.slotin@instana.com>
2020-03-04 11:31:15 +0100

    Use instana.{Min,Max}Int64 constants in tests instead of raw values

--- util_internal_test.go
+++ util_internal_test.go
@@ -10,23 +10,23 @@ import (
 )
 
 // Trace IDs (and Span IDs) are based on Java Signed Long datatype
-const MinUint64 = uint64(0)
-const MaxUint64 = uint64(18446744073709551615)
-const MinInt64 = int64(-9223372036854775808)
-const MaxInt64 = int64(9223372036854775807)
+const (
+	MinInt64 int64 = -9223372036854775808
+	MaxInt64 int64 = 9223372036854775807
+)
 
 func TestGeneratedIDRange(t *testing.T) {
 	var count = 10000
 	for index := 0; index < count; index++ {
 		id := randomID()
-		assert.True(t, id <= 9223372036854775807, "Generated ID is out of bounds (+)")
-		assert.True(t, id >= -9223372036854775808, "Generated ID is out of bounds (-)")
+		assert.True(t, id <= MaxInt64, "Generated ID is out of bounds (+)")
+		assert.True(t, id >= MinInt64, "Generated ID is out of bounds (-)")
 	}
 }
 
 func TestIDConversionBackForth(t *testing.T) {
-	maxID := int64(9223372036854775807)
-	minID := int64(-9223372036854775808)
+	maxID := int64(MaxInt64)
+	minID := int64(MinInt64)
 	maxHex := "7fffffffffffffff"
 	minHex := "8000000000000000"
 
