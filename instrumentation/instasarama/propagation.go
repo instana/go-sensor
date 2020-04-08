421dfc303284d1204b2614a511743f1aea8186cb
Andrey Slotin <andrey.slotin@instana.com>
2020-04-08 17:12:03 +0200

    Add instasarama.ConsumerMessageCarrier trace context propagator

--- instrumentation/instasarama/propagation.go
+++ instrumentation/instasarama/propagation.go
@@ -115,6 +115,108 @@ func (c ProducerMessageCarrier) indexOf(key []byte) (int, bool) {
 	return -1, false
 }
 
+// ConsumerMessageCarrier is a trace context carrier that extracts Instana OpenTracing
+// headers from Kafka consumer messages
+type ConsumerMessageCarrier struct {
+	Message *sarama.ConsumerMessage
+}
+
+// Set implements opentracing.TextMapWriter for ConsumerMessageCarrier
+func (c ConsumerMessageCarrier) Set(key, val string) {
+	switch strings.ToLower(key) {
+	case instana.FieldT:
+		if len(val) > 32 {
+			return // ignore hex-encoded trace IDs longer than 128 bit
+		}
+
+		traceContext := PackTraceContextHeader(val, "")
+		if i, ok := c.indexOf(fieldCKey); ok {
+			// preserve the trace ID if the trace context header already present
+			existingC := c.Message.Headers[i].Value
+			if len(existingC) >= 16 {
+				copy(traceContext[16:], existingC[16:])
+			}
+		}
+
+		c.addOrReplaceHeader(fieldCKey, traceContext)
+	case instana.FieldS:
+		if len(val) > 16 {
+			return // ignore hex-encoded span IDs longer than 64 bit
+		}
+
+		traceContext := PackTraceContextHeader("", val)
+		if i, ok := c.indexOf(fieldCKey); ok {
+			// preserve the span ID if the trace context header already present
+			existingC := c.Message.Headers[i].Value
+			if len(existingC) >= 16 {
+				copy(traceContext[:16], existingC[:16])
+			}
+		}
+
+		c.addOrReplaceHeader(fieldCKey, traceContext)
+	case instana.FieldL:
+		c.addOrReplaceHeader(fieldLKey, PackTraceLevelHeader(val))
+	}
+}
+
+// ForeachKey implements opentracing.TextMapReader for ConsumerMessageCarrier
+func (c ConsumerMessageCarrier) ForeachKey(handler func(key, val string) error) error {
+	for _, header := range c.Message.Headers {
+		if header == nil {
+			continue
+		}
+
+		switch {
+		case bytes.EqualFold(header.Key, fieldCKey):
+			traceID, spanID, err := UnpackTraceContextHeader(header.Value)
+			if err != nil {
+				return fmt.Errorf("malformed %q header: %s", header.Key, err)
+			}
+
+			if err := handler(instana.FieldT, string(traceID)); err != nil {
+				return err
+			}
+
+			if err := handler(instana.FieldS, string(spanID)); err != nil {
+				return err
+			}
+		case bytes.EqualFold(header.Key, fieldLKey):
+			val, err := UnpackTraceLevelHeader(header.Value)
+			if err != nil {
+				return fmt.Errorf("malformed %q header: %s", header.Key, err)
+			}
+
+			if err := handler(instana.FieldL, val); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (c ConsumerMessageCarrier) addOrReplaceHeader(key, val []byte) {
+	if i, ok := c.indexOf(key); ok {
+		c.Message.Headers[i].Value = val
+		return
+	}
+
+	c.Message.Headers = append(c.Message.Headers, &sarama.RecordHeader{Key: key, Value: val})
+}
+
+func (c ConsumerMessageCarrier) indexOf(key []byte) (int, bool) {
+	for i, header := range c.Message.Headers {
+		if header == nil {
+			continue
+		}
+
+		if bytes.EqualFold(key, header.Key) {
+			return i, true
+		}
+	}
+
+	return -1, false
+}
+
 func extractTraceSpanID(msg *sarama.ProducerMessage) (string, string, error) {
 	var traceID, spanID string
 	err := ProducerMessageCarrier{msg}.ForeachKey(func(k, v string) error {
--- instrumentation/instasarama/propagation_test.go
+++ instrumentation/instasarama/propagation_test.go
@@ -384,3 +384,384 @@ func TestProducerMessageCarrier_ForeachKey_Error(t *testing.T) {
 		return errors.New("something went wrong")
 	}))
 }
+
+func TestConsumerMessageCarrier_Set_FieldT(t *testing.T) {
+	var msg sarama.ConsumerMessage
+	c := instasarama.ConsumerMessageCarrier{&msg}
+
+	c.Set(instana.FieldT, "deadbeefdeadbeef")
+	assert.Equal(t, []*sarama.RecordHeader{
+		{
+			Key: []byte(instasarama.FieldC),
+			Value: []byte{
+				// trace id
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef,
+				// spanid
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			},
+		},
+	}, msg.Headers)
+}
+
+func TestConsumerMessageCarrier_Update_FieldT(t *testing.T) {
+	examples := map[string]struct {
+		Value    string
+		Headers  []*sarama.RecordHeader
+		Expected []*sarama.RecordHeader
+	}{
+		"existing has trace id only": {
+			Value: "deadbeef",
+			Headers: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0xab, 0xcd, 0xef, 0x12, 0xab, 0xcd, 0xef, 0x12,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					},
+				},
+				nil,
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+			Expected: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					},
+				},
+				nil,
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+		},
+		"existing has span id only": {
+			Value: "deadbeef",
+			Headers: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+					},
+				},
+				nil,
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+			Expected: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+					},
+				},
+				nil,
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+		},
+		"existing has trace and span id": {
+			Value: "deadbeef",
+			Headers: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0xab, 0xcd, 0xef, 0x12, 0xab, 0xcd, 0xef, 0x12,
+						// span id
+						0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12, 0x34,
+					},
+				},
+				nil,
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+			Expected: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+						// span id
+						0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12, 0x34,
+					},
+				},
+				nil,
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+		},
+	}
+
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			msg := sarama.ConsumerMessage{Headers: example.Headers}
+			c := instasarama.ConsumerMessageCarrier{&msg}
+
+			c.Set(instana.FieldT, example.Value)
+			assert.ElementsMatch(t, example.Expected, msg.Headers)
+		})
+	}
+}
+
+func TestConsumerMessageCarrier_Set_FieldS(t *testing.T) {
+	var msg sarama.ConsumerMessage
+	c := instasarama.ConsumerMessageCarrier{&msg}
+
+	c.Set(instana.FieldS, "deadbeef")
+	assert.Equal(t, []*sarama.RecordHeader{
+		{
+			Key: []byte(instasarama.FieldC),
+			Value: []byte{
+				// trace id
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				// span id
+				0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+			},
+		},
+	}, msg.Headers)
+}
+
+func TestConsumerMessageCarrier_Update_FieldS(t *testing.T) {
+	examples := map[string]struct {
+		Value    string
+		Headers  []*sarama.RecordHeader
+		Expected []*sarama.RecordHeader
+	}{
+		"existing has trace id only": {
+			Value: "deadbeef",
+			Headers: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					},
+				},
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+			Expected: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+					},
+				},
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+		},
+		"existing has span id only": {
+			Value: "deadbeef",
+			Headers: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+					},
+				},
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+			Expected: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+					},
+				},
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+		},
+		"existing has trace and span id": {
+			Value: "deadbeef",
+			Headers: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+					},
+				},
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+			Expected: []*sarama.RecordHeader{
+				{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+				{
+					Key: []byte("x_instana_c"),
+					Value: []byte{
+						// trace id
+						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+						// span id
+						0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+					},
+				},
+				{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			},
+		},
+	}
+
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			msg := sarama.ConsumerMessage{Headers: example.Headers}
+			c := instasarama.ConsumerMessageCarrier{&msg}
+
+			c.Set(instana.FieldS, example.Value)
+			assert.ElementsMatch(t, example.Expected, msg.Headers)
+		})
+	}
+}
+
+func TestConsumerMessageCarrier_Set_FieldL(t *testing.T) {
+	examples := map[string][]*sarama.RecordHeader{
+		"0": []*sarama.RecordHeader{
+			{Key: []byte(instasarama.FieldL), Value: []byte{0x00}},
+		},
+		"1": []*sarama.RecordHeader{
+			{Key: []byte(instasarama.FieldL), Value: []byte{0x01}},
+		},
+	}
+
+	for value, expected := range examples {
+		t.Run(value, func(t *testing.T) {
+			msg := sarama.ConsumerMessage{Headers: expected}
+			c := instasarama.ConsumerMessageCarrier{&msg}
+
+			c.Set(instana.FieldL, value)
+			assert.Equal(t, expected, msg.Headers)
+		})
+	}
+}
+
+func TestConsumerMessageCarrier_Update_FieldL(t *testing.T) {
+	msg := sarama.ConsumerMessage{
+		Headers: []*sarama.RecordHeader{
+			{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+			{Key: []byte("x_instana_l"), Value: []byte{0x00}},
+			{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+		},
+	}
+	c := instasarama.ConsumerMessageCarrier{&msg}
+
+	c.Set(instana.FieldL, "1")
+	assert.ElementsMatch(t, []*sarama.RecordHeader{
+		{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+		{Key: []byte("x_instana_l"), Value: []byte{0x01}},
+		{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+	}, msg.Headers)
+}
+
+func TestConsumerMessageCarrier_ForeachKey(t *testing.T) {
+	msg := sarama.ConsumerMessage{
+		Headers: []*sarama.RecordHeader{
+			{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+			{
+				Key: []byte("x_instana_c"),
+				Value: []byte{
+					// trace id
+					0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+					// span id
+					0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+				},
+			},
+			nil,
+			{Key: []byte("X_CUSTOM_2"), Value: []byte("value2")},
+			{Key: []byte("x_INSTANA_L"), Value: []byte{0x01}},
+		},
+	}
+	c := instasarama.ConsumerMessageCarrier{&msg}
+
+	var collected []struct{ Key, Value string }
+	require.NoError(t, c.ForeachKey(func(k, v string) error {
+		collected = append(collected, struct{ Key, Value string }{k, v})
+		return nil
+	}))
+
+	assert.ElementsMatch(t, []struct{ Key, Value string }{
+		{Key: instana.FieldT, Value: "abcdef12"},
+		{Key: instana.FieldS, Value: "deadbeef"},
+		{Key: instana.FieldL, Value: "1"},
+	}, collected)
+}
+
+func TestConsumerMessageCarrier_ForeachKey_NoTracingHeaders(t *testing.T) {
+	msg := sarama.ConsumerMessage{
+		Headers: []*sarama.RecordHeader{
+			{Key: []byte("X_CUSTOM_1"), Value: []byte("value1")},
+		},
+	}
+	c := instasarama.ConsumerMessageCarrier{&msg}
+
+	var collected []struct{ Key, Value string }
+	require.NoError(t, c.ForeachKey(func(k, v string) error {
+		collected = append(collected, struct{ Key, Value string }{k, v})
+		return nil
+	}))
+
+	assert.Empty(t, collected)
+}
+
+func TestConsumerMessageCarrier_ForeachKey_Error(t *testing.T) {
+	msg := sarama.ConsumerMessage{
+		Headers: []*sarama.RecordHeader{
+			{
+				Key: []byte("x_instana_c"),
+				Value: []byte{
+					// trace id
+					0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+					0x00, 0x00, 0x00, 0x00, 0xab, 0xcd, 0xef, 0x12,
+					// span id
+					0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0xef,
+				},
+			},
+			{Key: []byte("x_INSTANA_L"), Value: []byte{0x01}},
+		},
+	}
+	c := instasarama.ConsumerMessageCarrier{&msg}
+
+	assert.Error(t, c.ForeachKey(func(k, v string) error {
+		return errors.New("something went wrong")
+	}))
+}
