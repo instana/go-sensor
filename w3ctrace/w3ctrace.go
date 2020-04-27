8d4560027685083bcee5a8c79317489ec28f6eb5
Andrey Slotin <andrey.slotin@instana.com>
2020-04-27 17:29:15 +0200

    Add w3ctrace.Extract() that extracts w3c trace context from HTTP headers

---
+++ w3ctrace/w3ctrace.go
@@ -0,0 +1,46 @@
+package w3ctrace
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+const (
+	// W3C trace context header names as defined by https://www.w3.org/TR/trace-context/
+	TraceParentHeader = "traceparent"
+	TraceStateHeader  = "tracestate"
+)
+
+var ErrContextNotFound = errors.New("no w3c context")
+
+// Context represents the W3C trace context
+type Context struct {
+	RawParent string
+	RawState  string
+}
+
+// Extract extracts the W3C trace context from HTTP headers. Returns ErrContextNotFound if
+// provided value doesn't contain traceparent header.
+func Extract(headers http.Header) (Context, error) {
+	var tr Context
+
+	for k, v := range headers {
+		if len(v) == 0 {
+			continue
+		}
+
+		switch {
+		case strings.EqualFold(k, TraceParentHeader):
+			tr.RawParent = v[0]
+		case strings.EqualFold(k, TraceStateHeader):
+			tr.RawState = v[0]
+		}
+	}
+
+	if tr.RawParent == "" {
+		return tr, ErrContextNotFound
+	}
+
+	return tr, nil
+}
---
+++ w3ctrace/w3ctrace_test.go
@@ -0,0 +1,52 @@
+package w3ctrace_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/instana/go-sensor/w3ctrace"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	exampleTraceParent = "00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01"
+	exampleTraceState  = "vendorname1=opaqueValue1 , vendorname2=opaqueValue2"
+)
+
+func TestExtract(t *testing.T) {
+	examples := map[string]struct {
+		ParentHeader string
+		StateHeader  string
+	}{
+		"lower case": {"traceparent", "tracestate"},
+		"upper case": {"TRACEPARENT", "TRACESTATE"},
+		"mixed case": {"Traceparent", "Tracestate"},
+	}
+
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			headers := http.Header{}
+			// set raw headers to preserve header name case
+			headers[example.ParentHeader] = []string{exampleTraceParent}
+			headers[example.StateHeader] = []string{exampleTraceState}
+
+			tr, err := w3ctrace.Extract(headers)
+			require.NoError(t, err)
+
+			assert.Equal(t, w3ctrace.Context{
+				RawParent: exampleTraceParent,
+				RawState:  exampleTraceState,
+			}, tr)
+		})
+	}
+}
+
+func TestExtract_NoContext(t *testing.T) {
+	headers := http.Header{}
+	headers.Set(w3ctrace.TraceStateHeader, exampleTraceState)
+
+	_, err := w3ctrace.Extract(headers)
+	assert.Equal(t, w3ctrace.ErrContextNotFound, err)
+}
+RawParent
