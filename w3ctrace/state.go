2b26c1965629ccde6f7f0d2b004f275fb385df3a
Andrey Slotin <andrey.slotin@instana.com>
2020-05-04 13:52:33 +0200

    Organize w3ctrace code into separate files

--- w3ctrace/state.go
+++ w3ctrace/state.go
@@ -2,80 +2,8 @@ package w3ctrace
 
 import (
 	"bytes"
-	"errors"
-	"net/http"
 	"strings"
 )
-
-const (
-	// The max number of items in `tracestate` as defined by https://www.w3.org/TR/trace-context/#tracestate-header-field-values
-	MaxStateEntries = 32
-
-	// W3C trace context header names as defined by https://www.w3.org/TR/trace-context/
-	TraceParentHeader = "traceparent"
-	TraceStateHeader  = "tracestate"
-)
-
-var (
-	ErrContextNotFound    = errors.New("no w3c context")
-	ErrContextCorrupted   = errors.New("corrupted w3c context")
-	ErrUnsupportedVersion = errors.New("unsupported w3c context version")
-)
-
-// Context represents the W3C trace context
-type Context struct {
-	RawParent string
-	RawState  string
-}
-
-// Extract extracts the W3C trace context from HTTP headers. Returns ErrContextNotFound if
-// provided value doesn't contain traceparent header.
-func Extract(headers http.Header) (Context, error) {
-	var tr Context
-
-	for k, v := range headers {
-		if len(v) == 0 {
-			continue
-		}
-
-		switch {
-		case strings.EqualFold(k, TraceParentHeader):
-			tr.RawParent = v[0]
-		case strings.EqualFold(k, TraceStateHeader):
-			tr.RawState = v[0]
-		}
-	}
-
-	if tr.RawParent == "" {
-		return tr, ErrContextNotFound
-	}
-
-	return tr, nil
-}
-
-// Inject adds the w3c trace context headers, overriding any previously set values
-func Inject(trCtx Context, headers http.Header) {
-	// delete existing headers ignoring the header name case
-	for k := range headers {
-		if strings.EqualFold(k, TraceParentHeader) || strings.EqualFold(k, TraceStateHeader) {
-			delete(headers, k)
-		}
-	}
-
-	headers.Set(TraceParentHeader, trCtx.RawParent)
-	headers.Set(TraceStateHeader, trCtx.RawState)
-}
-
-// State parses RawState and returns the corresponding list.
-// It silently discards malformed state. To check errors use ParseState().
-func (trCtx Context) State() State {
-	st, err := ParseState(trCtx.RawState)
-	if err != nil {
-		return State{}
-	}
-
-	return st
-}
 
 // State is list of key=value pairs representing vendor-specific data in the trace context
 type State []string
--- w3ctrace/state_test.go
+++ w3ctrace/state_test.go
@@ -2,89 +2,12 @@ package w3ctrace_test
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/instana/go-sensor/w3ctrace"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-const (
-	exampleTraceParent = "00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01"
-	exampleTraceState  = "vendorname1=opaqueValue1 , vendorname2=opaqueValue2"
-)
-
-func TestExtract(t *testing.T) {
-	examples := map[string]struct {
-		ParentHeader string
-		StateHeader  string
-	}{
-		"lower case": {"traceparent", "tracestate"},
-		"upper case": {"TRACEPARENT", "TRACESTATE"},
-		"mixed case": {"Traceparent", "Tracestate"},
-	}
-
-	for name, example := range examples {
-		t.Run(name, func(t *testing.T) {
-			headers := http.Header{}
-			// set raw headers to preserve header name case
-			headers[example.ParentHeader] = []string{exampleTraceParent}
-			headers[example.StateHeader] = []string{exampleTraceState}
-
-			tr, err := w3ctrace.Extract(headers)
-			require.NoError(t, err)
-
-			assert.Equal(t, w3ctrace.Context{
-				RawParent: exampleTraceParent,
-				RawState:  exampleTraceState,
-			}, tr)
-		})
-	}
-}
-
-func TestExtract_NoContext(t *testing.T) {
-	headers := http.Header{}
-	headers.Set(w3ctrace.TraceStateHeader, exampleTraceState)
-
-	_, err := w3ctrace.Extract(headers)
-	assert.Equal(t, w3ctrace.ErrContextNotFound, err)
-}
-
-func TestInject(t *testing.T) {
-	examples := map[string]http.Header{
-		"add": {
-			"Authorization": []string{"Basic 123"},
-		},
-		"update": {
-			"Authorization": []string{"Basic 123"},
-			"traceparent":   []string{"00-abcdef1-01"},
-			"TraceState":    []string{"x=y"},
-		},
-	}
-
-	for name, headers := range examples {
-		t.Run(name, func(t *testing.T) {
-			w3ctrace.Inject(w3ctrace.Context{
-				RawParent: exampleTraceParent,
-				RawState:  exampleTraceState,
-			}, headers)
-
-			assert.Equal(t, "Basic 123", headers.Get("Authorization"))
-			assert.Equal(t, exampleTraceParent, headers.Get(w3ctrace.TraceParentHeader))
-			assert.Equal(t, exampleTraceState, headers.Get(w3ctrace.TraceStateHeader))
-		})
-	}
-}
-
-func TestContext_State(t *testing.T) {
-	trCtx := w3ctrace.Context{
-		RawParent: exampleTraceParent,
-		RawState:  exampleTraceState,
-	}
-
-	assert.Equal(t, w3ctrace.State{"vendorname1=opaqueValue1", "vendorname2=opaqueValue2"}, trCtx.State())
-}
 
 func TestParseState(t *testing.T) {
 	examples := map[string]struct {
