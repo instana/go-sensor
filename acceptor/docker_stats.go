0c720e6b484ab3ec4cd5d2218748cc1ecbcf9796
Andrey Slotin <andrey.slotin@instana.com>
2020-09-10 17:59:08 +0200

    Calculate Docker stats deltas

---
+++ acceptor/docker_stats.go
@@ -0,0 +1,199 @@
+package acceptor
+
+import "github.com/instana/go-sensor/docker"
+
+// DockerNetworkStatsDelta represents the difference between two network interface stats
+type DockerNetworkStatsDelta struct {
+	Bytes   int `json:"bytes,omitempty"`
+	Packets int `json:"packets,omitempty"`
+	Dropped int `json:"dropped,omitempty"`
+	Errors  int `json:"errors,omitempty"`
+}
+
+// IsZero returns true is there is no difference between interface stats
+func (d DockerNetworkStatsDelta) IsZero() bool {
+	return d.Bytes == 0 && d.Packets == 0 && d.Dropped == 0 && d.Errors == 0
+}
+
+// DockerNetworkStatsDelta represents the difference between two network interface stats
+type DockerNetworkAggregatedStatsDelta struct {
+	Rx *DockerNetworkStatsDelta `json:"rx,omitempty"`
+	Tx *DockerNetworkStatsDelta `json:"tx,omitempty"`
+}
+
+// NewDockerNetworkAggregatedStatsDelta calculates the aggregated difference between two snapshots
+// of network interface stats. It returns nil if aggregated stats for both snapshots are equal.
+func NewDockerNetworkAggregatedStatsDelta(prev, next map[string]docker.ContainerNetworkStats) *DockerNetworkAggregatedStatsDelta {
+	var rxDelta, txDelta DockerNetworkStatsDelta
+
+	for _, stats := range next {
+		rxDelta.Bytes += stats.RxBytes
+		rxDelta.Packets += stats.RxPackets
+		rxDelta.Dropped += stats.RxDropped
+		rxDelta.Errors += stats.RxErrors
+
+		txDelta.Bytes += stats.TxBytes
+		txDelta.Packets += stats.TxPackets
+		txDelta.Dropped += stats.TxDropped
+		txDelta.Errors += stats.TxErrors
+	}
+
+	for _, stats := range prev {
+		rxDelta.Bytes -= stats.RxBytes
+		rxDelta.Packets -= stats.RxPackets
+		rxDelta.Dropped -= stats.RxDropped
+		rxDelta.Errors -= stats.RxErrors
+
+		txDelta.Bytes -= stats.TxBytes
+		txDelta.Packets -= stats.TxPackets
+		txDelta.Dropped -= stats.TxDropped
+		txDelta.Errors -= stats.TxErrors
+	}
+
+	if rxDelta.IsZero() && txDelta.IsZero() {
+		return nil
+	}
+
+	var delta DockerNetworkAggregatedStatsDelta
+	if !rxDelta.IsZero() {
+		delta.Rx = &rxDelta
+	}
+	if !txDelta.IsZero() {
+		delta.Tx = &txDelta
+	}
+
+	return &delta
+}
+
+// DockerCPUStatsDelta represents the difference between two CPU usage stats
+type DockerCPUStatsDelta struct {
+	Total           float64 `json:"total_usage,omitempty"`
+	User            float64 `json:"user_usage,omitempty"`
+	System          float64 `json:"system_usage,omitempty"`
+	ThrottlingCount int     `json:"throttling_count,omitempty"`
+	ThrottlingTime  int     `json:"throttling_time,omitempty"`
+}
+
+// NewDockerCPUStatsDelta calculates the difference between two CPU usage stats. It returns nil if stats are equal.
+func NewDockerCPUStatsDelta(prev, next docker.ContainerCPUStats) *DockerCPUStatsDelta {
+	if prev == next {
+		return nil
+	}
+
+	delta := DockerCPUStatsDelta{
+		ThrottlingCount: next.Throttling.Periods - prev.Throttling.Periods,
+		ThrottlingTime:  next.Throttling.Time - prev.Throttling.Time,
+	}
+
+	if systemDelta := next.System - prev.System; systemDelta > 0 {
+		if totalDelta := next.Usage.Total - prev.Usage.Total; totalDelta > 0 {
+			delta.Total = (float64(totalDelta) / float64(systemDelta)) * float64(next.OnlineCPUs)
+		}
+		if kernelDelta := next.Usage.Kernel - prev.Usage.Kernel; kernelDelta > 0 {
+			delta.System = (float64(kernelDelta) / float64(systemDelta)) * float64(next.OnlineCPUs)
+		}
+		if userDelta := next.Usage.User - prev.Usage.User; userDelta > 0 {
+			delta.User = (float64(userDelta) / float64(systemDelta)) * float64(next.OnlineCPUs)
+		}
+	}
+
+	return &delta
+}
+
+// DockerMemoryStatsUpdate represents the memory stats that have changed since the last measurement
+type DockerMemoryStatsUpdate struct {
+	ActiveAnon   *int `json:"active_anon,omitempty"`
+	ActiveFile   *int `json:"active_file,omitempty"`
+	InactiveAnon *int `json:"inactive_anon,omitempty"`
+	InactiveFile *int `json:"inactive_file,omitempty"`
+	TotalCache   *int `json:"total_cache,omitempty"`
+	TotalRss     *int `json:"total_rss,omitempty"`
+	Usage        *int `json:"usage,omitempty"`
+	MaxUsage     *int `json:"max_usage,omitempty"`
+	Limit        *int `json:"limit,omitempty"`
+}
+
+// NewDockerMemoryStatsUpdate returns the fields that have been updated since the last measurement.
+// It returns nil if nothing has changed.
+func NewDockerMemoryStatsUpdate(prev, next docker.ContainerMemoryStats) *DockerMemoryStatsUpdate {
+	if prev == next {
+		return nil
+	}
+
+	var delta DockerMemoryStatsUpdate
+	if prev.Usage != next.Usage {
+		delta.Usage = &next.Usage
+	}
+	if prev.MaxUsage != next.MaxUsage {
+		delta.MaxUsage = &next.MaxUsage
+	}
+	if prev.Limit != next.Limit {
+		delta.Limit = &next.Limit
+	}
+
+	if prev.Stats == next.Stats {
+		return &delta
+	}
+
+	if prev.Stats.ActiveAnon != next.Stats.ActiveAnon {
+		delta.ActiveAnon = &next.Stats.ActiveAnon
+	}
+	if prev.Stats.ActiveFile != next.Stats.ActiveFile {
+		delta.ActiveFile = &next.Stats.ActiveFile
+	}
+	if prev.Stats.InactiveAnon != next.Stats.InactiveAnon {
+		delta.InactiveAnon = &next.Stats.InactiveAnon
+	}
+	if prev.Stats.InactiveFile != next.Stats.InactiveFile {
+		delta.InactiveFile = &next.Stats.InactiveFile
+	}
+	if prev.Stats.TotalCache != next.Stats.TotalCache {
+		delta.TotalCache = &next.Stats.TotalCache
+	}
+	if prev.Stats.TotalRss != next.Stats.TotalRss {
+		delta.TotalRss = &next.Stats.TotalRss
+	}
+
+	return &delta
+}
+
+// DockerMemoryStatsDelta represents the difference between two block I/O usage stats
+type DockerBlockIOStatsDelta struct {
+	Read  int `json:"blk_read,omitempty"`
+	Write int `json:"blk_write,omitempty"`
+}
+
+// IsZero returns true if both usage stats are equal
+func (d DockerBlockIOStatsDelta) IsZero() bool {
+	return d.Read == 0 && d.Write == 0
+}
+
+// NewDockerMemoryStatsDelta sums up block I/O reads and writes and calculates the difference between two stat snapshots.
+// It returns nil if aggregated stats are equal.
+func NewDockerBlockIOStatsDelta(prev, next docker.ContainerBlockIOStats) *DockerBlockIOStatsDelta {
+	var delta DockerBlockIOStatsDelta
+
+	for _, stat := range next.ServiceBytes {
+		switch stat.Operation {
+		case docker.BlockIOReadOp:
+			delta.Read += stat.Value
+		case docker.BlockIOWriteOp:
+			delta.Write += stat.Value
+		}
+	}
+
+	for _, stat := range prev.ServiceBytes {
+		switch stat.Operation {
+		case docker.BlockIOReadOp:
+			delta.Read -= stat.Value
+		case docker.BlockIOWriteOp:
+			delta.Write -= stat.Value
+		}
+	}
+
+	if delta.IsZero() {
+		return nil
+	}
+
+	return &delta
+}
---
+++ acceptor/docker_stats_test.go
@@ -0,0 +1,240 @@
+package acceptor_test
+
+import (
+	"testing"
+
+	"github.com/instana/go-sensor/acceptor"
+	"github.com/instana/go-sensor/docker"
+	"github.com/instana/testify/assert"
+)
+
+func TestNewDockerNetworkAggregatedStatsDelta(t *testing.T) {
+	stats := map[string]docker.ContainerNetworkStats{
+		"eth0": {
+			RxBytes:   1,
+			RxDropped: 10,
+			RxErrors:  100,
+			RxPackets: 1000,
+			TxBytes:   10000,
+			TxDropped: 100000,
+			TxErrors:  1000000,
+			TxPackets: 10000000,
+		},
+		"eth1": {
+			RxBytes:   2,
+			RxDropped: 20,
+			RxErrors:  200,
+			RxPackets: 2000,
+			TxBytes:   20000,
+			TxDropped: 200000,
+			TxErrors:  2000000,
+			TxPackets: 20000000,
+		},
+		"eth2": {
+			RxBytes:   3,
+			RxDropped: 30,
+			RxErrors:  300,
+			RxPackets: 3000,
+			TxBytes:   30000,
+			TxDropped: 300000,
+			TxErrors:  3000000,
+			TxPackets: 30000000,
+		},
+	}
+
+	t.Run("equal", func(t *testing.T) {
+		assert.Nil(t, acceptor.NewDockerNetworkAggregatedStatsDelta(stats, stats))
+	})
+
+	t.Run("increase", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerNetworkAggregatedStatsDelta{
+				Rx: &acceptor.DockerNetworkStatsDelta{
+					Bytes:   6,
+					Dropped: 60,
+					Errors:  600,
+					Packets: 6000,
+				},
+				Tx: &acceptor.DockerNetworkStatsDelta{
+					Bytes:   60000,
+					Dropped: 600000,
+					Errors:  6000000,
+					Packets: 60000000,
+				},
+			},
+			acceptor.NewDockerNetworkAggregatedStatsDelta(nil, stats),
+		)
+	})
+
+	t.Run("decrease", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerNetworkAggregatedStatsDelta{
+				Rx: &acceptor.DockerNetworkStatsDelta{
+					Bytes:   -6,
+					Dropped: -60,
+					Errors:  -600,
+					Packets: -6000,
+				},
+				Tx: &acceptor.DockerNetworkStatsDelta{
+					Bytes:   -60000,
+					Dropped: -600000,
+					Errors:  -6000000,
+					Packets: -60000000,
+				},
+			},
+			acceptor.NewDockerNetworkAggregatedStatsDelta(stats, nil),
+		)
+	})
+}
+
+func TestNewDockerCPUStatsDelta(t *testing.T) {
+	stats := docker.ContainerCPUStats{
+		Usage: docker.CPUUsageStats{
+			Total:  1,
+			User:   10,
+			Kernel: 100,
+		},
+		Throttling: docker.CPUThrottlingStats{
+			Periods: 1000,
+			Time:    10000,
+		},
+		System:     100000,
+		OnlineCPUs: 16,
+	}
+
+	t.Run("equal", func(t *testing.T) {
+		assert.Nil(t, acceptor.NewDockerCPUStatsDelta(stats, stats))
+	})
+
+	t.Run("increase", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerCPUStatsDelta{
+				Total:           0.00016,
+				User:            0.0016,
+				System:          0.016,
+				ThrottlingCount: 1000,
+				ThrottlingTime:  10000,
+			},
+			acceptor.NewDockerCPUStatsDelta(docker.ContainerCPUStats{}, stats),
+		)
+	})
+
+	t.Run("decrease", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerCPUStatsDelta{
+				Total:           0,
+				User:            0,
+				System:          0,
+				ThrottlingCount: -1000,
+				ThrottlingTime:  -10000,
+			},
+			acceptor.NewDockerCPUStatsDelta(stats, docker.ContainerCPUStats{}),
+		)
+	})
+}
+
+func TestNewDockerMemoryStatsDelta(t *testing.T) {
+	stats := docker.ContainerMemoryStats{
+		Stats: docker.MemoryStats{
+			ActiveAnon:   1,
+			ActiveFile:   10,
+			InactiveAnon: 100,
+			InactiveFile: 1000,
+			TotalRss:     10000,
+			TotalCache:   100000,
+		},
+		MaxUsage: 1000000,
+		Usage:    10000000,
+		Limit:    100000000,
+	}
+
+	t.Run("equal", func(t *testing.T) {
+		assert.Nil(t, acceptor.NewDockerMemoryStatsUpdate(stats, stats))
+	})
+
+	t.Run("changed", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerMemoryStatsUpdate{
+				ActiveAnon:   &stats.Stats.ActiveAnon,
+				ActiveFile:   &stats.Stats.ActiveFile,
+				InactiveAnon: &stats.Stats.InactiveAnon,
+				InactiveFile: &stats.Stats.InactiveFile,
+				TotalRss:     &stats.Stats.TotalRss,
+				TotalCache:   &stats.Stats.TotalCache,
+				MaxUsage:     &stats.MaxUsage,
+				Usage:        &stats.Usage,
+				Limit:        &stats.Limit,
+			},
+			acceptor.NewDockerMemoryStatsUpdate(docker.ContainerMemoryStats{}, stats),
+		)
+	})
+
+	t.Run("changed some", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerMemoryStatsUpdate{
+				ActiveFile:   &stats.Stats.ActiveFile,
+				InactiveFile: &stats.Stats.InactiveFile,
+				TotalCache:   &stats.Stats.TotalCache,
+				MaxUsage:     &stats.MaxUsage,
+				Limit:        &stats.Limit,
+			},
+			acceptor.NewDockerMemoryStatsUpdate(docker.ContainerMemoryStats{
+				Stats: docker.MemoryStats{
+					ActiveAnon:   1,
+					ActiveFile:   20,
+					InactiveAnon: 100,
+					InactiveFile: 2000,
+					TotalRss:     10000,
+					TotalCache:   200000,
+				},
+				MaxUsage: 2000000,
+				Usage:    10000000,
+				Limit:    200000000,
+			}, stats),
+		)
+	})
+}
+
+func TestNewDockerBlockIOStatsDelta(t *testing.T) {
+	stats := docker.ContainerBlockIOStats{
+		ServiceBytes: []docker.BlockIOOpStats{
+			{Operation: docker.BlockIOReadOp, Value: 1},
+			{Operation: docker.BlockIOWriteOp, Value: 10},
+			{Operation: docker.BlockIOReadOp, Value: 100},
+		},
+	}
+
+	t.Run("equal", func(t *testing.T) {
+		assert.Nil(t, acceptor.NewDockerBlockIOStatsDelta(stats, stats))
+	})
+
+	t.Run("increase", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerBlockIOStatsDelta{
+				Read:  101,
+				Write: 10,
+			},
+			acceptor.NewDockerBlockIOStatsDelta(docker.ContainerBlockIOStats{}, stats),
+		)
+	})
+
+	t.Run("decrease", func(t *testing.T) {
+		assert.Equal(t,
+			&acceptor.DockerBlockIOStatsDelta{
+				Read:  -101,
+				Write: -10,
+			},
+			acceptor.NewDockerBlockIOStatsDelta(stats, docker.ContainerBlockIOStats{}),
+		)
+	})
+
+	t.Run("equal, shuffled ops", func(t *testing.T) {
+		assert.Nil(t, acceptor.NewDockerBlockIOStatsDelta(stats, docker.ContainerBlockIOStats{
+			ServiceBytes: []docker.BlockIOOpStats{
+				{Operation: docker.BlockIOReadOp, Value: 100},
+				{Operation: docker.BlockIOReadOp, Value: 1},
+				{Operation: docker.BlockIOWriteOp, Value: 10},
+			},
+		}))
+	})
+}
