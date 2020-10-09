bfe821e0d3a9e6706fe0c8f1cfe43140d5860f47
Andrey Slotin <andrey.slotin@instana.com>
2020-10-09 15:25:19 +0200

    Instrument (*cloud.google.com/go/pubsub.Subscription).Receive()

--- instrumentation/cloud.google.com/go/pubsub/subscription.go
+++ instrumentation/cloud.google.com/go/pubsub/subscription.go
@@ -1,8 +1,12 @@
 package pubsub
 
 import (
+	"context"
+
 	"cloud.google.com/go/pubsub"
 	instana "github.com/instana/go-sensor"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 )
 
 // Subscription is an instrumented wrapper for cloud.google.com/go/pubsub.Subscription that traces Receive() calls
@@ -16,6 +20,44 @@ type Subscription struct {
 	topicID   string
 
 	sensor *instana.Sensor
+}
+
+func (sub *Subscription) fetchTopicID(ctx context.Context) string {
+	if sub.topicID != "" {
+		return sub.topicID
+	}
+
+	if cfg, err := sub.Subscription.Config(ctx); err == nil {
+		sub.topicID = cfg.Topic.ID()
+	}
+
+	return sub.topicID
+}
+
+// Receive wraps the Receive() call of the underlying cloud.google.com/go/pubsub.Subscription starting a new
+// entry span using the provided instana.Sensor and ensuring the trace continuation.
+//
+// See https://pkg.go.dev/cloud.google.com/go/pubsub?tab=doc#Subscription.Receive for furter details on wrapped method.
+func (sub *Subscription) Receive(ctx context.Context, f func(context.Context, *pubsub.Message)) error {
+	return sub.Subscription.Receive(ctx, func(mCtx context.Context, msg *pubsub.Message) {
+		opts := []opentracing.StartSpanOption{
+			ext.SpanKindConsumer,
+			opentracing.Tags{
+				"gcps.op":     "consume",
+				"gcps.projid": sub.projectID,
+				"gcps.top":    sub.fetchTopicID(ctx),
+				"gcps.sub":    sub.ID(),
+			},
+		}
+		if spCtx, err := sub.sensor.Tracer().Extract(opentracing.TextMap, opentracing.TextMapCarrier(msg.Attributes)); err == nil {
+			opts = append(opts, opentracing.ChildOf(spCtx))
+		}
+
+		sp := sub.sensor.Tracer().StartSpan("gcps", opts...)
+		defer sp.Finish()
+
+		f(instana.ContextWithSpan(mCtx, sp), msg)
+	})
 }
 
 // SubscriptionIterator is a wrapper for cloud.google.com/go/pubsub.SubscriptionIterator that retrieves and instruments
---
+++ instrumentation/cloud.google.com/go/pubsub/subscription_test.go
@@ -0,0 +1,160 @@
+package pubsub_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	gpubsub "cloud.google.com/go/pubsub"
+	"cloud.google.com/go/pubsub/pstest"
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/cloud.google.com/go/pubsub"
+	"github.com/instana/testify/assert"
+	"github.com/instana/testify/require"
+	"google.golang.org/api/option"
+	pb "google.golang.org/genproto/googleapis/pubsub/v1"
+)
+
+func TestSubscription_Receive(t *testing.T) {
+	pstest.SetMinAckDeadline(1 * time.Second)
+	defer pstest.ResetMinAckDeadline()
+
+	recorder := instana.NewTestRecorder()
+	tracer := instana.NewTracerWithEverything(instana.DefaultOptions(), recorder)
+
+	srv, conn, teardown, err := setupMockServer()
+	require.NoError(t, err)
+	defer teardown()
+
+	top, err := srv.GServer.CreateTopic(context.Background(), &pb.Topic{
+		Name: "projects/test-project/topics/test-topic",
+	})
+	require.NoError(t, err)
+
+	_, err = srv.GServer.CreateSubscription(context.Background(), &pb.Subscription{
+		Topic:              top.Name,
+		Name:               "projects/test-project/subscriptions/test-subscription",
+		AckDeadlineSeconds: 10,
+	})
+	require.NoError(t, err)
+
+	msgID := srv.Publish(top.Name, []byte("test message"), map[string]string{
+		"x-instana-t": "0000000000001234",
+		"x-instana-s": "0000000000005678",
+		"x-instana-l": "1",
+	})
+
+	client, err := pubsub.NewClient(
+		context.Background(),
+		"test-project",
+		instana.NewSensorWithTracer(tracer),
+		option.WithGRPCConn(conn),
+	)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	client.Subscription("test-subscription").Receive(ctx, func(ctx context.Context, msg *gpubsub.Message) {
+		assert.Equal(t, msgID, msg.ID)
+		msg.Ack()
+		cancel()
+	})
+
+	assert.Equal(t, context.Canceled, ctx.Err())
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 1)
+
+	gcpsSpan := spans[0]
+
+	// trace continuation
+	assert.EqualValues(t, 0x1234, gcpsSpan.TraceID)
+	assert.EqualValues(t, 0x5678, gcpsSpan.ParentID)
+	assert.NotEqual(t, gcpsSpan.ParentID, gcpsSpan.SpanID)
+
+	// span tags
+	assert.Equal(t, "gcps", gcpsSpan.Name)
+	assert.EqualValues(t, instana.EntrySpanKind, gcpsSpan.Kind)
+	assert.Equal(t, 0, gcpsSpan.Ec)
+
+	require.IsType(t, instana.GCPPubSubSpanData{}, gcpsSpan.Data)
+
+	data := gcpsSpan.Data.(instana.GCPPubSubSpanData)
+	assert.Equal(t, instana.GCPPubSubSpanTags{
+		Operation:    "consume",
+		ProjectID:    "test-project",
+		Topic:        "test-topic",
+		Subscription: "test-subscription",
+	}, data.Tags)
+}
+
+func TestSubscription_Receive_NoTrace(t *testing.T) {
+	pstest.SetMinAckDeadline(1 * time.Second)
+	defer pstest.ResetMinAckDeadline()
+
+	recorder := instana.NewTestRecorder()
+	tracer := instana.NewTracerWithEverything(instana.DefaultOptions(), recorder)
+
+	srv, conn, teardown, err := setupMockServer()
+	require.NoError(t, err)
+	defer teardown()
+
+	top, err := srv.GServer.CreateTopic(context.Background(), &pb.Topic{
+		Name: "projects/test-project/topics/test-topic",
+	})
+	require.NoError(t, err)
+
+	_, err = srv.GServer.CreateSubscription(context.Background(), &pb.Subscription{
+		Topic:              top.Name,
+		Name:               "projects/test-project/subscriptions/test-subscription",
+		AckDeadlineSeconds: 10,
+	})
+	require.NoError(t, err)
+
+	msgID := srv.Publish(top.Name, []byte("test message"), nil)
+
+	client, err := pubsub.NewClient(
+		context.Background(),
+		"test-project",
+		instana.NewSensorWithTracer(tracer),
+		option.WithGRPCConn(conn),
+	)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	client.Subscription("test-subscription").Receive(ctx, func(ctx context.Context, msg *gpubsub.Message) {
+		assert.Equal(t, msgID, msg.ID)
+		msg.Ack()
+		cancel()
+	})
+
+	assert.Equal(t, context.Canceled, ctx.Err())
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 1)
+
+	gcpsSpan := spans[0]
+
+	// new trace started
+	assert.NotEmpty(t, gcpsSpan.TraceID)
+	assert.Empty(t, gcpsSpan.ParentID)
+	assert.NotEmpty(t, gcpsSpan.SpanID)
+
+	// span tags
+	assert.Equal(t, "gcps", gcpsSpan.Name)
+	assert.EqualValues(t, instana.EntrySpanKind, gcpsSpan.Kind)
+	assert.Equal(t, 0, gcpsSpan.Ec)
+
+	require.IsType(t, instana.GCPPubSubSpanData{}, gcpsSpan.Data)
+
+	data := gcpsSpan.Data.(instana.GCPPubSubSpanData)
+	assert.Equal(t, instana.GCPPubSubSpanTags{
+		Operation:    "consume",
+		ProjectID:    "test-project",
+		Topic:        "test-topic",
+		Subscription: "test-subscription",
+	}, data.Tags)
+}
