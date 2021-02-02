72f677cf39f10fabdf38b09908069f668d7cad25
Andrey Slotin <andrey.slotin@instana.com>
2021-02-02 16:29:40 +0100

    Only send the lower 4 bytes of trace IDs to the agent

--- json_span.go
+++ json_span.go
@@ -219,7 +219,7 @@ func (sp Span) MarshalJSON() ([]byte, error) {
 		CorrelationType string         `json:"crtp,omitempty"`
 		CorrelationID   string         `json:"crid,omitempty"`
 	}{
-		FormatLongID(sp.TraceIDHi, sp.TraceID),
+		FormatID(sp.TraceID),
 		parentID,
 		FormatID(sp.SpanID),
 		sp.Timestamp,
