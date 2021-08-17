2b959482824582c714ac4417b55f5cc9ac5c72c1
Andrii Kushch <andrii.kushch@instana.com>
2021-08-17 09:34:01 +0200

    Add gorilla mux instrumentation

---
+++ instrumentation/instamux/handler.go
@@ -0,0 +1,28 @@
+// (c) Copyright IBM Corp. 2021
+// (c) Copyright Instana Inc. 2016
+
+// +build go1.12
+
+package instamux
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	instana "github.com/instana/go-sensor"
+)
+
+func AddMiddleware(sensor *instana.Sensor, router *mux.Router) {
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			pathTemplate, err := mux.CurrentRoute(r).GetPathTemplate()
+			if err != nil {
+				pathTemplate = ""
+			}
+
+			instana.TracingHandlerFunc(sensor, pathTemplate, func(writer http.ResponseWriter, request *http.Request) {
+				next.ServeHTTP(writer, request)
+			})(w, r)
+		})
+	})
+}
