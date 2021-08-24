b34a6410b925b57495d1df24a4e180107135aa93
Andrey Slotin <andrey.slotin@instana.com>
2021-08-24 14:39:14 +0200

    Instrument github.com/julienschmidt/httprouter.Router

---
+++ instrumentation/instahttprouter/router.go
@@ -0,0 +1,93 @@
+// (c) Copyright IBM Corp. 2021
+// (c) Copyright Instana Inc. 2021
+
+package instahttprouter
+
+import (
+	"context"
+	"net/http"
+
+	instana "github.com/instana/go-sensor"
+	"github.com/julienschmidt/httprouter"
+)
+
+// Wrap returns an instrumented instance of a httprouter.Router that
+// instruments HTTP handlers with Instana upon registration.
+func Wrap(r *httprouter.Router, sensor *instana.Sensor) *wrappedRouter {
+	return &wrappedRouter{
+		Router: r,
+		sensor: sensor,
+	}
+}
+
+type wrappedRouter struct {
+	*httprouter.Router
+	sensor *instana.Sensor
+}
+
+// GET is a shortcut for router.Handle(http.MethodGet, path, handle)
+func (r *wrappedRouter) GET(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodGet, path, handle)
+}
+
+// HEAD is a shortcut for router.Handle(http.MethodHead, path, handle)
+func (r *wrappedRouter) HEAD(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodHead, path, handle)
+}
+
+// OPTIONS is a shortcut for router.Handle(http.MethodOptions, path, handle)
+func (r *wrappedRouter) OPTIONS(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodOptions, path, handle)
+}
+
+// POST is a shortcut for router.Handle(http.MethodPost, path, handle)
+func (r *wrappedRouter) POST(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodPost, path, handle)
+}
+
+// PUT is a shortcut for router.Handle(http.MethodPut, path, handle)
+func (r *wrappedRouter) PUT(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodPut, path, handle)
+}
+
+// PATCH is a shortcut for router.Handle(http.MethodPatch, path, handle)
+func (r *wrappedRouter) PATCH(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodPatch, path, handle)
+}
+
+// DELETE is a shortcut for router.Handle(http.MethodDelete, path, handle)
+func (r *wrappedRouter) DELETE(path string, handle httprouter.Handle) {
+	r.Handle(http.MethodDelete, path, handle)
+}
+
+// Handle instruments and registers a new request handle with the given path and method.
+//
+// For details please refer to the (*httprouter.Router).Handle() documentation:
+// https://pkg.go.dev/github.com/julienschmidt/httprouter#Router.Handle
+func (r *wrappedRouter) Handle(method, path string, handle httprouter.Handle) {
+	r.Router.HandlerFunc(method, path, instana.TracingHandlerFunc(r.sensor, path, func(w http.ResponseWriter, req *http.Request) {
+		handle(w, req, httprouter.ParamsFromContext(req.Context()))
+	}))
+}
+
+// Handler is an adapter which allows the usage of an uninstrumented http.Handler as a
+// request handle.
+// The Params are available in the request context under ParamsKey.
+func (r *wrappedRouter) Handler(method, path string, handler http.Handler) {
+	r.Handle(method, path,
+		func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+			if len(p) > 0 {
+				ctx := req.Context()
+				ctx = context.WithValue(ctx, httprouter.ParamsKey, p)
+				req = req.WithContext(ctx)
+			}
+			handler.ServeHTTP(w, req)
+		},
+	)
+}
+
+// HandlerFunc is an adapter which allows the usage of an uninstrumented http.HandlerFunc as a
+// request handle.
+func (r *wrappedRouter) HandlerFunc(method, path string, handler http.HandlerFunc) {
+	r.Handler(method, path, handler)
+}
---
+++ instrumentation/instahttprouter/router_test.go
@@ -0,0 +1,308 @@
+// (c) Copyright IBM Corp. 2021
+// (c) Copyright Instana Inc. 2021
+
+package instahttprouter_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/instahttprouter"
+	"github.com/instana/go-sensor/secrets"
+	"github.com/instana/go-sensor/w3ctrace"
+	"github.com/instana/testify/assert"
+	"github.com/instana/testify/require"
+	"github.com/julienschmidt/httprouter"
+	"github.com/opentracing/opentracing-go"
+)
+
+func TestRouter_Handle_StartTrace(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	tracer := instana.NewTracerWithEverything(&instana.Options{
+		Tracer: instana.TracerOptions{
+			Secrets:                secrets.NewEqualsMatcher("secret"),
+			CollectableHTTPHeaders: []string{"X-Custom-1"},
+		},
+	}, recorder)
+	sensor := instana.NewSensorWithTracer(tracer)
+
+	r := instahttprouter.Wrap(httprouter.New(), sensor)
+	r.Handle(http.MethodGet, "/user/:id", func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		assert.Equal(t, httprouter.Params{
+			httprouter.Param{Key: "id", Value: "id1"},
+		}, params)
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/user/id1?q=test&secret=password", nil)
+	req.Header.Set("X-Custom-1", "custom1")
+	req.Header.Set("X-Custom-2", "custom2")
+
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusNoContent, resp.Code)
+
+	assert.NotEmpty(t, resp.Header().Get(instana.FieldT))
+	assert.NotEmpty(t, resp.Header().Get(instana.FieldS))
+	assert.NotEmpty(t, resp.Header().Get(instana.FieldL))
+	assert.NotEmpty(t, resp.Header().Get(w3ctrace.TraceParentHeader))
+	assert.NotEmpty(t, resp.Header().Get(w3ctrace.TraceStateHeader))
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 1)
+
+	span := spans[0]
+
+	assert.NotEmpty(t, span.TraceID)
+	assert.Empty(t, span.ParentID)
+	assert.NotEmpty(t, span.SpanID)
+
+	assert.Equal(t, "g.http", span.Name)
+	assert.Equal(t, 0, span.Ec)
+
+	require.IsType(t, instana.HTTPSpanData{}, span.Data)
+	data := span.Data.(instana.HTTPSpanData)
+
+	assert.Equal(t, instana.HTTPSpanTags{
+		Status:       http.StatusNoContent,
+		Method:       http.MethodGet,
+		Path:         "/user/id1",
+		PathTemplate: "/user/:id",
+		Host:         "example.com",
+		Params:       "q=test&secret=%3Credacted%3E",
+		Headers:      map[string]string{"X-Custom-1": "custom1"},
+	}, data.Tags)
+}
+
+func TestRouter_TracePropagation(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(instana.NewTracerWithEverything(instana.DefaultOptions(), recorder))
+
+	r := instahttprouter.Wrap(httprouter.New(), sensor)
+	r.Handle(http.MethodGet, "/user/:id", func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		if sp, ok := instana.SpanFromContext(req.Context()); ok {
+			defer sp.Tracer().StartSpan("handler", opentracing.ChildOf(sp.Context())).Finish()
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/user/id1?q=test&secret=password", nil)
+
+	parentSpan := instana.NewRootSpanContext()
+	sensor.Tracer().Inject(parentSpan, opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(req.Header))
+
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusNoContent, resp.Code)
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 2)
+
+	handlerSp, routerSp := spans[0], spans[1]
+
+	assert.Equal(t, "g.http", routerSp.Name)
+	assert.Equal(t, parentSpan.TraceID, routerSp.TraceID)
+	assert.Equal(t, parentSpan.TraceIDHi, routerSp.TraceIDHi)
+	assert.Equal(t, parentSpan.SpanID, routerSp.ParentID)
+
+	assert.Equal(t, "sdk", handlerSp.Name)
+	assert.Equal(t, routerSp.TraceID, handlerSp.TraceID)
+	assert.Equal(t, routerSp.TraceIDHi, handlerSp.TraceIDHi)
+	assert.Equal(t, routerSp.SpanID, handlerSp.ParentID)
+}
+
+func TestRouter_Handle_ErrorHandling(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(instana.NewTracerWithEverything(instana.DefaultOptions(), recorder))
+
+	r := instahttprouter.Wrap(httprouter.New(), sensor)
+	r.Handle(http.MethodGet, "/user/:id", func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+	})
+
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, httptest.NewRequest(http.MethodGet, "/user/id1", nil))
+
+	assert.Equal(t, http.StatusInternalServerError, resp.Code)
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 2)
+
+	span, logSpan := spans[0], spans[1]
+
+	assert.NotEmpty(t, span.TraceID)
+	assert.Empty(t, span.ParentID)
+	assert.NotEmpty(t, span.SpanID)
+
+	assert.Equal(t, "g.http", span.Name)
+	assert.Equal(t, 1, span.Ec)
+
+	require.IsType(t, instana.HTTPSpanData{}, span.Data)
+	data := span.Data.(instana.HTTPSpanData)
+
+	assert.Equal(t, instana.HTTPSpanTags{
+		Status:       http.StatusInternalServerError,
+		Method:       http.MethodGet,
+		Path:         "/user/id1",
+		PathTemplate: "/user/:id",
+		Host:         "example.com",
+		Error:        "Internal Server Error",
+	}, data.Tags)
+
+	assert.Equal(t, span.TraceID, logSpan.TraceID)
+	assert.Equal(t, span.SpanID, logSpan.ParentID)
+	assert.Equal(t, "log.go", logSpan.Name)
+
+	// assert that log message has been recorded within the span interval
+	assert.GreaterOrEqual(t, logSpan.Timestamp, span.Timestamp)
+	assert.LessOrEqual(t, logSpan.Duration, span.Duration)
+
+	require.IsType(t, instana.LogSpanData{}, logSpan.Data)
+	logData := logSpan.Data.(instana.LogSpanData)
+
+	assert.Equal(t, instana.LogSpanTags{
+		Level:   "ERROR",
+		Message: `error: "Internal Server Error"`,
+	}, logData.Tags)
+}
+
+func TestRouter_Handle_PanicHandling(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(instana.NewTracerWithEverything(instana.DefaultOptions(), recorder))
+
+	r := instahttprouter.Wrap(httprouter.New(), sensor)
+	r.Handle(http.MethodGet, "/user/:id", func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+		panic("something went wrong")
+	})
+
+	resp := httptest.NewRecorder()
+	assert.Panics(t, func() {
+		r.ServeHTTP(resp, httptest.NewRequest(http.MethodGet, "/user/id1", nil))
+	})
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 2)
+
+	span, logSpan := spans[0], spans[1]
+
+	assert.NotEmpty(t, span.TraceID)
+	assert.Empty(t, span.ParentID)
+	assert.NotEmpty(t, span.SpanID)
+
+	assert.Equal(t, "g.http", span.Name)
+	assert.Equal(t, 1, span.Ec)
+
+	require.IsType(t, instana.HTTPSpanData{}, span.Data)
+	data := span.Data.(instana.HTTPSpanData)
+
+	assert.Equal(t, instana.HTTPSpanTags{
+		Status:       http.StatusInternalServerError,
+		Method:       http.MethodGet,
+		Path:         "/user/id1",
+		PathTemplate: "/user/:id",
+		Host:         "example.com",
+		Error:        "something went wrong",
+	}, data.Tags)
+
+	assert.Equal(t, span.TraceID, logSpan.TraceID)
+	assert.Equal(t, span.SpanID, logSpan.ParentID)
+	assert.Equal(t, "log.go", logSpan.Name)
+
+	// assert that log message has been recorded within the span interval
+	assert.GreaterOrEqual(t, logSpan.Timestamp, span.Timestamp)
+	assert.LessOrEqual(t, logSpan.Duration, span.Duration)
+
+	require.IsType(t, instana.LogSpanData{}, logSpan.Data)
+	logData := logSpan.Data.(instana.LogSpanData)
+
+	assert.Equal(t, instana.LogSpanTags{
+		Level:   "ERROR",
+		Message: `error: "something went wrong"`,
+	}, logData.Tags)
+}
+
+func TestRouter_Helpers(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(instana.NewTracerWithEverything(instana.DefaultOptions(), recorder))
+
+	r := instahttprouter.Wrap(httprouter.New(), sensor)
+
+	examples := map[string]func(httprouter.Handle) string{
+		"GET": func(h httprouter.Handle) string {
+			r.GET("/get", h)
+
+			return "/get"
+		},
+		"POST": func(h httprouter.Handle) string {
+			r.POST("/post", h)
+
+			return "/post"
+		},
+		"HEAD": func(h httprouter.Handle) string {
+			r.HEAD("/head", h)
+
+			return "/head"
+		},
+		"OPTIONS": func(h httprouter.Handle) string {
+			r.OPTIONS("/options", h)
+
+			return "/options"
+		},
+		"DELETE": func(h httprouter.Handle) string {
+			r.DELETE("/delete", h)
+
+			return "/delete"
+		},
+		"PUT": func(h httprouter.Handle) string {
+			r.PUT("/put", h)
+
+			return "/put"
+		},
+		"PATCH": func(h httprouter.Handle) string {
+			r.PATCH("/patch", h)
+
+			return "/patch"
+		},
+	}
+
+	for method, register := range examples {
+		t.Run(method, func(t *testing.T) {
+			path := register(func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+				w.WriteHeader(http.StatusNoContent)
+			})
+
+			resp := httptest.NewRecorder()
+			r.ServeHTTP(resp, httptest.NewRequest(method, path, nil))
+
+			assert.Equal(t, http.StatusNoContent, resp.Code)
+
+			spans := recorder.GetQueuedSpans()
+			require.Len(t, spans, 1)
+
+			span := spans[0]
+
+			assert.NotEmpty(t, span.TraceID)
+			assert.Empty(t, span.ParentID)
+			assert.NotEmpty(t, span.SpanID)
+
+			assert.Equal(t, "g.http", span.Name)
+			assert.Equal(t, 0, span.Ec)
+
+			require.IsType(t, instana.HTTPSpanData{}, span.Data)
+			data := span.Data.(instana.HTTPSpanData)
+
+			assert.Equal(t, instana.HTTPSpanTags{
+				Status: http.StatusNoContent,
+				Method: method,
+				Path:   path,
+				Host:   "example.com",
+			}, data.Tags)
+		})
+	}
+}
