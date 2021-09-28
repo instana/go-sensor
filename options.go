5e78d88e94a2bffcda6c280f9b5c14e95af4facf
Andrey Slotin <andrey.slotin@instana.com>
2021-09-28 11:10:38 +0200

    Document instana.Options fields

--- options.go
+++ options.go
@@ -10,16 +10,37 @@ import (
 
 // Options allows the user to configure the to-be-initialized sensor
 type Options struct {
-	Service                     string
-	AgentHost                   string
-	AgentPort                   int
-	MaxBufferedSpans            int
+	// Service is the global service name that will be used to identify the program in the Instana backend
+	Service string
+	// AgentHost is the Instana host agent host name
+	//
+	// Note: This setting has no effect in serverless environments. To specify the serverless acceptor endpoint,
+	// INSTANA_ENDPOINT_URL env var. See https://www.instana.com/docs/reference/environment_variables/#serverless-monitoring
+	// for more details.
+	AgentHost string
+	// AgentPort is the Instana host agent port
+	//
+	// Note: This setting has no effect in serverless environments. To specify the serverless acceptor endpoint,
+	// INSTANA_ENDPOINT_URL env var. See https://www.instana.com/docs/reference/environment_variables/#serverless-monitoring
+	// for more details.
+	AgentPort int
+	// MaxBufferedSpans is the maximum number of spans to buffer
+	MaxBufferedSpans int
+	// ForceTransmissionStartingAt is the number of spans to collect before flushing the buffer to the agent
 	ForceTransmissionStartingAt int
-	LogLevel                    int
-	EnableAutoProfile           bool
-	MaxBufferedProfiles         int
-	IncludeProfilerFrames       bool
-	Tracer                      TracerOptions
+	// LogLevel is the initial logging level for the logger used by Instana tracer. The valid log levels are
+	// logger.{Error,Warn,Info,Debug}Level provided by the github.com/instana/go-sensor/logger package.
+	//
+	// Note: this setting is only used to initialize the default logger and has no effect if a custom logger is set via instana.SetLogger()
+	LogLevel int
+	// EnableAutoProfile enables automatic continuous process profiling when set to true
+	EnableAutoProfile bool
+	// MaxBufferedProfiles is the maximum number of profiles to buffer
+	MaxBufferedProfiles int
+	// IncludeProfilerFrames is whether to include profiler calls into the profile or not
+	IncludeProfilerFrames bool
+	// Tracer contains tracer-specific configuration used by all tracers
+	Tracer TracerOptions
 
 	disableW3CTraceCorrelation bool
 }
