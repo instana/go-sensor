d8e4fbda9a2d2600f4478a972976d549fe56282d
Andrey Slotin <andrey.slotin@instana.com>
2021-10-06 14:47:02 +0200

    Trace MongoDB command execution using the event.CommandMonitor

--- instrumentation/instamongo/instamongo.go
+++ instrumentation/instamongo/instamongo.go
@@ -5,23 +5,205 @@ package instamongo
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
 
 	instana "github.com/instana/go-sensor"
+	"github.com/opentracing/opentracing-go"
+	otlog "github.com/opentracing/opentracing-go/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
 	"go.mongodb.org/mongo-driver/event"
 )
 
-type CommandMonitor struct {
-	sensor *instana.Sensor
+var unmarshalReg *bsoncodec.Registry
+
+func init() {
+	rb := bson.NewRegistryBuilder()
+	rb.RegisterTypeMapEntry(bsontype.EmbeddedDocument, reflect.TypeOf(bson.M{}))
+
+	unmarshalReg = rb.Build()
 }
 
-func NewCommandMonitor(sensor *instana.Sensor) *CommandMonitor {
-	return &CommandMonitor{
+type commandMonitor struct {
+	sensor *instana.Sensor
+	spans  *spanRegistry
+}
+
+// NewCommandMonitor creates a new event.CommandMonitor to be used in mongo.ClientOptions.
+// This monitor instruments mongo.Client with Instana tracing any requests made to the database.
+func NewCommandMonitor(sensor *instana.Sensor) *event.CommandMonitor {
+	mon := &commandMonitor{
 		sensor: sensor,
+		spans:  newSpanRegistry(),
+	}
+
+	return &event.CommandMonitor{
+		Started:   mon.Started,
+		Succeeded: mon.Succeeded,
+		Failed:    mon.Failed,
 	}
 }
 
-func (*CommandMonitor) Started(ctx context.Context, evt *event.CommandStartedEvent) {}
+// Started traces command start initiating a new span. This span is finalized whenever either
+// Succeeded() or Failed() method is called with an event containing the same RequestID.
+func (m *commandMonitor) Started(ctx context.Context, evt *event.CommandStartedEvent) {
+	parent, ok := instana.SpanFromContext(ctx)
+	if !ok {
+		return
+	}
 
-func (*CommandMonitor) Succeeded(ctx context.Context, evt *event.CommandSucceededEvent) {}
+	ns := evt.DatabaseName
+	if collection, ok := evt.Command.Lookup(evt.CommandName).StringValueOK(); ok {
+		ns += "." + collection
+	}
 
-func (*CommandMonitor) Failed(ctx context.Context, evt *event.CommandFailedEvent) {}
+	sp := m.sensor.Tracer().StartSpan(
+		"mongo",
+		opentracing.ChildOf(parent.Context()),
+		m.extractSpanTags(evt),
+	)
+
+	m.spans.Add(evt.RequestID, sp)
+}
+
+// Succeeded finalizes the command span started by Started()
+func (m *commandMonitor) Succeeded(ctx context.Context, evt *event.CommandSucceededEvent) {
+	sp, ok := m.spans.Remove(evt.RequestID)
+	if !ok {
+		return
+	}
+
+	sp.Finish()
+}
+
+// Failed finalizes the command span started by Started() and logs the failure reason
+func (m *commandMonitor) Failed(ctx context.Context, evt *event.CommandFailedEvent) {
+	sp, ok := m.spans.Remove(evt.RequestID)
+	if !ok {
+		return
+	}
+
+	sp.SetTag("mongo.error", evt.Failure)
+	sp.LogFields(otlog.Object("error", evt.Failure))
+
+	sp.Finish()
+}
+
+func (m *commandMonitor) extractSpanTags(evt *event.CommandStartedEvent) opentracing.Tags {
+	ns := evt.DatabaseName
+	if collection, ok := evt.Command.Lookup(evt.CommandName).StringValueOK(); ok {
+		ns += "." + collection
+	}
+
+	tags := opentracing.Tags{
+		"mongo.service":   extractAddress(evt.ConnectionID),
+		"mongo.namespace": ns,
+		"mongo.command":   evt.CommandName,
+	}
+
+	if doc := evt.Command.Lookup("query"); doc.Validate() == nil {
+		if data, err := bsonToJSON(doc); err != nil {
+			m.sensor.Logger().Warn("failed to marshal mongodb ", evt.CommandName, " query to json: ", err)
+		} else {
+			tags["mongo.query"] = string(data)
+		}
+	}
+
+	if doc := evt.Command.Lookup("filter"); doc.Validate() == nil {
+		if data, err := bsonToJSON(evt.Command.Lookup("filter")); err != nil {
+			m.sensor.Logger().Warn("failed to marshal mongodb ", evt.CommandName, " filter to json: ", err)
+		} else {
+			tags["mongo.filter"] = string(data)
+		}
+	}
+
+	if doc, ok := extractCommandDocument(evt.CommandName, evt.Command); ok {
+		if data, err := bsonToJSON(doc); err != nil {
+			m.sensor.Logger().Warn("failed to marshal mongodb ", evt.CommandName, " document to json: ", err)
+		} else {
+			tags["mongo.json"] = string(data)
+		}
+	}
+
+	return tags
+}
+
+// extractAddress extracts the MongoDB server address (either host:port or a path to UNIX socket) from connection ID by
+// truncating the optional connection number at the end of the value.
+//
+// See newConnection() in https://github.com/mongodb/mongo-go-driver/blob/master/x/mongo/driver/topology/connection.go for details
+func extractAddress(connID string) string {
+	for i := len(connID) - 1; i >= 0; i-- {
+		switch connID[i] {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		case '-':
+			return connID[0:i]
+		default:
+			return connID
+		}
+	}
+
+	return connID
+}
+
+func extractCommandDocument(cmdName string, cmdBody bson.Raw) (bson.RawValue, bool) {
+	var v bson.RawValue
+
+	switch cmdName {
+	case "insert":
+		v = cmdBody.Lookup("documents")
+	case "update":
+		v = cmdBody.Lookup("updates")
+	case "delete":
+		v = cmdBody.Lookup("deletes")
+	case "aggregate":
+		v = cmdBody.Lookup("pipeline")
+	case "mapReduce":
+		doc := make(map[string]string, 2)
+
+		if mapV := cmdBody.Lookup("map"); mapV.Validate() == nil {
+			if s, ok := stringOrJavaScriptOK(mapV); ok {
+				doc["map"] = s
+			}
+		}
+
+		if reduceV := cmdBody.Lookup("reduce"); reduceV.Validate() == nil {
+			if s, ok := stringOrJavaScriptOK(reduceV); ok {
+				doc["reduce"] = s
+			}
+		}
+
+		typ, data, err := bson.MarshalValue(doc)
+		if err != nil {
+			return bson.RawValue{}, false
+		}
+
+		v = bson.RawValue{
+			Type:  typ,
+			Value: data,
+		}
+	}
+
+	return v, v.Validate() == nil
+}
+
+func bsonToJSON(data bson.RawValue) ([]byte, error) {
+	var v interface{}
+
+	if err := data.UnmarshalWithRegistry(unmarshalReg, &v); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal bson value: %s", err)
+	}
+
+	return json.Marshal(v)
+}
+
+func stringOrJavaScriptOK(v bson.RawValue) (string, bool) {
+	if s, ok := v.JavaScriptOK(); ok {
+		return s, true
+	}
+
+	return v.StringValueOK()
+}
---
+++ instrumentation/instamongo/instamongo_test.go
@@ -0,0 +1,432 @@
+package instamongo_test
+
+import (
+	"context"
+	"testing"
+
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/instamongo"
+	"github.com/instana/testify/assert"
+	"github.com/instana/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/event"
+)
+
+func TestNewCommandMonitor_Succeeded(t *testing.T) {
+	examples := map[string]struct {
+		Database     string
+		Command      string
+		Data         interface{}
+		ConnectionID string
+		Expected     instana.MongoDBSpanTags
+	}{
+		"find": {
+			Database: "testing-db",
+			Command:  "find",
+			Data: bson.M{
+				"find":   "records",
+				"filter": bson.D{bson.E{"value", "42"}, bson.E{"valid", true}},
+			},
+			ConnectionID: "localhost:27017",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "localhost:27017",
+				Namespace: "testing-db.records",
+				Command:   "find",
+				Filter:    `{"value": "42", "valid": true}`,
+			},
+		},
+		"insert": {
+			Database: "testing-db",
+			Command:  "insert",
+			Data: bson.M{
+				"insert": "records",
+				"documents": bson.A{
+					bson.D{bson.E{"value", 1}},
+					bson.D{bson.E{"value", 2}},
+				},
+			},
+			ConnectionID: "localhost:27017",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "localhost:27017",
+				Namespace: "testing-db.records",
+				Command:   "insert",
+				JSON:      `[{"value": 1}, {"value": 2}]`,
+			},
+		},
+		"update": {
+			Database: "testing-db",
+			Command:  "update",
+			Data: bson.M{
+				"update": "records",
+				"query":  bson.D{bson.E{"valid", false}},
+				"updates": bson.A{
+					bson.D{
+						bson.E{"q", bson.D{bson.E{"valid", false}}},
+						bson.E{"u", bson.D{
+							bson.E{"valid", true},
+							bson.E{"value", 0},
+						}},
+					},
+				},
+			},
+			ConnectionID: "localhost:27017",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "localhost:27017",
+				Namespace: "testing-db.records",
+				Command:   "update",
+				JSON:      `[{"q": {"valid": false}, "u": {"value": 0, "valid": true}}]`,
+			},
+		},
+		"delete": {
+			Database: "testing-db",
+			Command:  "delete",
+			Data: bson.M{
+				"delete": "records",
+				"deletes": bson.A{
+					bson.D{bson.E{"q", bson.D{bson.E{"valid", false}}}},
+					bson.D{bson.E{"q", bson.D{bson.E{"value", 0}}}, bson.E{"limit", 1}},
+				},
+			},
+			ConnectionID: "localhost:27017",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "localhost:27017",
+				Namespace: "testing-db.records",
+				Command:   "delete",
+				JSON:      `[{"q": {"valid": false}}, {"q": {"value": 0}, "limit": 1}]`,
+			},
+		},
+		"aggregate": {
+			Database: "testing-db",
+			Command:  "aggregate",
+			Data: bson.M{
+				"aggregate": "records",
+				"pipeline": bson.A{
+					bson.D{
+						bson.E{"$search", bson.D{bson.E{"valid", true}}},
+						bson.E{"$sum", "value"},
+					},
+				},
+			},
+			ConnectionID: "localhost:27017",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "localhost:27017",
+				Namespace: "testing-db.records",
+				Command:   "aggregate",
+				JSON:      `[{"$search": {"valid": true}, "$sum": "value"}]`,
+			},
+		},
+		"mapReduce": {
+			Database: "testing-db",
+			Command:  "mapReduce",
+			Data: bson.M{
+				"mapReduce": "records",
+				"map":       "function () { this.tags.forEach(function(z) { emit(z, 1); }); }",
+				"reduce":    "function (key, values) { return len(values); }",
+			},
+			ConnectionID: "localhost:27017",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "localhost:27017",
+				Namespace: "testing-db.records",
+				Command:   "mapReduce",
+				JSON:      `{"map": "function () { this.tags.forEach(function(z) { emit(z, 1); }); }", "reduce": "function (key, values) { return len(values); }"}`,
+			},
+		},
+		"network connection with id": {
+			Database: "testing-db",
+			Command:  "listDatabases",
+			Data: bson.M{
+				"listDatabases": 1,
+			},
+			ConnectionID: "mongo-host:12345-123",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "mongo-host:12345",
+				Namespace: "testing-db",
+				Command:   "listDatabases",
+			},
+		},
+		"unix socket": {
+			Database: "testing-db",
+			Command:  "listDatabases",
+			Data: bson.M{
+				"listDatabases": 1,
+			},
+			ConnectionID: "/var/run/mongo.sock",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "/var/run/mongo.sock",
+				Namespace: "testing-db",
+				Command:   "listDatabases",
+			},
+		},
+		"unix socket with id": {
+			Database: "testing-db",
+			Command:  "listDatabases",
+			Data: bson.M{
+				"listDatabases": 1,
+			},
+			ConnectionID: "/var/run/mongo.sock-123",
+			Expected: instana.MongoDBSpanTags{
+				Service:   "/var/run/mongo.sock",
+				Namespace: "testing-db",
+				Command:   "listDatabases",
+			},
+		},
+	}
+
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			recorder := instana.NewTestRecorder()
+			sensor := instana.NewSensorWithTracer(
+				instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+			)
+
+			m := instamongo.NewCommandMonitor(sensor)
+
+			sp := sensor.Tracer().StartSpan("testing")
+			ctx := instana.ContextWithSpan(context.Background(), sp)
+
+			m.Started(ctx, &event.CommandStartedEvent{
+				DatabaseName: example.Database,
+				Command:      marshalBSON(t, example.Data),
+				CommandName:  example.Command,
+				RequestID:    1,
+				ConnectionID: example.ConnectionID,
+			})
+
+			m.Succeeded(context.Background(), &event.CommandSucceededEvent{
+				CommandFinishedEvent: event.CommandFinishedEvent{
+					DurationNanos: 123,
+					CommandName:   example.Command,
+					RequestID:     1,
+					ConnectionID:  example.ConnectionID,
+				},
+				Reply: marshalBSON(t, bson.M{
+					"databases": bson.A{},
+				}),
+			})
+
+			sp.Finish()
+
+			spans := recorder.GetQueuedSpans()
+			require.Len(t, spans, 2)
+
+			dbSpan, parentSpan := spans[0], spans[1]
+
+			assert.Equal(t, parentSpan.TraceID, dbSpan.TraceID)
+			assert.Equal(t, parentSpan.TraceIDHi, dbSpan.TraceIDHi)
+			assert.Equal(t, parentSpan.SpanID, dbSpan.ParentID)
+
+			assert.Equal(t, "mongo", dbSpan.Name)
+			assert.EqualValues(t, instana.ExitSpanKind, dbSpan.Kind)
+			assert.Empty(t, dbSpan.Ec)
+
+			require.IsType(t, instana.MongoDBSpanData{}, dbSpan.Data)
+
+			data := dbSpan.Data.(instana.MongoDBSpanData)
+
+			// Check JSON fields separately to ignore formatting
+			if example.Expected.Query != "" {
+				assert.JSONEq(t, example.Expected.Query, data.Tags.Query)
+			}
+
+			if example.Expected.Filter != "" {
+				assert.JSONEq(t, example.Expected.Filter, data.Tags.Filter)
+			}
+
+			if example.Expected.JSON != "" {
+				assert.JSONEq(t, example.Expected.JSON, data.Tags.JSON)
+			}
+
+			data.Tags.Query = example.Expected.Query
+			data.Tags.Filter = example.Expected.Filter
+			data.Tags.JSON = example.Expected.JSON
+
+			assert.Equal(t, example.Expected, data.Tags)
+		})
+	}
+}
+
+func TestNewCommandMonitor_Succeeded_NotStarted(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(
+		instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+	)
+
+	m := instamongo.NewCommandMonitor(sensor)
+
+	m.Succeeded(context.Background(), &event.CommandSucceededEvent{
+		CommandFinishedEvent: event.CommandFinishedEvent{
+			DurationNanos: 123,
+			CommandName:   "listDatabases",
+			RequestID:     1,
+			ConnectionID:  "localhost:27017-1",
+		},
+		Reply: marshalBSON(t, bson.M{
+			"databases": bson.A{},
+		}),
+	})
+
+	assert.Empty(t, recorder.GetQueuedSpans())
+}
+
+func TestNewCommandMonitor_Succeeded_NotTraced(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(
+		instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+	)
+
+	m := instamongo.NewCommandMonitor(sensor)
+
+	m.Started(context.Background(), &event.CommandStartedEvent{
+		Command: marshalBSON(t, bson.M{
+			"listDatabases": 1,
+		}),
+		CommandName:  "listDatabases",
+		RequestID:    1,
+		ConnectionID: "localhost:27017-1",
+	})
+
+	m.Succeeded(context.Background(), &event.CommandSucceededEvent{
+		CommandFinishedEvent: event.CommandFinishedEvent{
+			DurationNanos: 123,
+			CommandName:   "listDatabases",
+			RequestID:     1,
+			ConnectionID:  "localhost:27017-1",
+		},
+		Reply: marshalBSON(t, bson.M{
+			"databases": bson.A{},
+		}),
+	})
+
+	assert.Empty(t, recorder.GetQueuedSpans())
+}
+
+func TestNewCommandMonitor_Failed(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(
+		instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+	)
+
+	m := instamongo.NewCommandMonitor(sensor)
+
+	sp := sensor.Tracer().StartSpan("testing")
+	ctx := instana.ContextWithSpan(context.Background(), sp)
+
+	m.Started(ctx, &event.CommandStartedEvent{
+		DatabaseName: "testing-db",
+		Command:      marshalBSON(t, bson.M{"listDatabases": 1}),
+		CommandName:  "listDatabases",
+		RequestID:    1,
+		ConnectionID: "mongo-host:12345-123",
+	})
+
+	m.Failed(context.Background(), &event.CommandFailedEvent{
+		CommandFinishedEvent: event.CommandFinishedEvent{
+			DurationNanos: 123,
+			CommandName:   "listDatabases",
+			RequestID:     1,
+			ConnectionID:  "mongo-host:12345-123",
+		},
+		Failure: "something went wrong",
+	})
+
+	sp.Finish()
+
+	spans := recorder.GetQueuedSpans()
+	require.Len(t, spans, 3)
+
+	dbSpan, logSpan, parentSpan := spans[0], spans[1], spans[2]
+
+	assert.Equal(t, parentSpan.TraceID, dbSpan.TraceID)
+	assert.Equal(t, parentSpan.TraceIDHi, dbSpan.TraceIDHi)
+	assert.Equal(t, parentSpan.SpanID, dbSpan.ParentID)
+
+	assert.Equal(t, "mongo", dbSpan.Name)
+	assert.EqualValues(t, instana.ExitSpanKind, dbSpan.Kind)
+	assert.Equal(t, 1, dbSpan.Ec)
+
+	require.IsType(t, instana.MongoDBSpanData{}, dbSpan.Data)
+
+	data := dbSpan.Data.(instana.MongoDBSpanData)
+	assert.Equal(t, instana.MongoDBSpanTags{
+		Service:   "mongo-host:12345",
+		Namespace: "testing-db",
+		Command:   "listDatabases",
+		Error:     "something went wrong",
+	}, data.Tags)
+
+	assert.Equal(t, dbSpan.TraceID, logSpan.TraceID)
+	assert.Equal(t, dbSpan.SpanID, logSpan.ParentID)
+	assert.Equal(t, "log.go", logSpan.Name)
+
+	// assert that log message has been recorded within the span interval
+	assert.GreaterOrEqual(t, logSpan.Timestamp, dbSpan.Timestamp)
+	assert.LessOrEqual(t, logSpan.Duration, dbSpan.Duration)
+
+	require.IsType(t, instana.LogSpanData{}, logSpan.Data)
+	logData := logSpan.Data.(instana.LogSpanData)
+
+	assert.Equal(t, instana.LogSpanTags{
+		Level:   "ERROR",
+		Message: `error: "something went wrong"`,
+	}, logData.Tags)
+}
+
+func TestNewCommandMonitor_Failed_NotStarted(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(
+		instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+	)
+
+	m := instamongo.NewCommandMonitor(sensor)
+
+	m.Failed(context.Background(), &event.CommandFailedEvent{
+		CommandFinishedEvent: event.CommandFinishedEvent{
+			DurationNanos: 123,
+			CommandName:   "listDatabases",
+			RequestID:     1,
+			ConnectionID:  "localhost:27017-1",
+		},
+		Failure: "something went wrong",
+	})
+
+	assert.Empty(t, recorder.GetQueuedSpans())
+}
+
+func TestNewCommandMonitor_Failed_NotTraced(t *testing.T) {
+	recorder := instana.NewTestRecorder()
+	sensor := instana.NewSensorWithTracer(
+		instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+	)
+
+	m := instamongo.NewCommandMonitor(sensor)
+
+	m.Started(context.Background(), &event.CommandStartedEvent{
+		Command: marshalBSON(t, bson.M{
+			"listDatabases": 1,
+		}),
+		CommandName:  "listDatabases",
+		RequestID:    1,
+		ConnectionID: "localhost:27017-1",
+	})
+
+	m.Failed(context.Background(), &event.CommandFailedEvent{
+		CommandFinishedEvent: event.CommandFinishedEvent{
+			DurationNanos: 123,
+			CommandName:   "listDatabases",
+			RequestID:     1,
+			ConnectionID:  "localhost:27017-1",
+		},
+		Failure: "something went wrong",
+	})
+
+	assert.Empty(t, recorder.GetQueuedSpans())
+}
+
+func marshalBSON(t *testing.T, data interface{}) bson.Raw {
+	t.Helper()
+
+	doc, err := bson.Marshal(data)
+	require.NoError(t, err)
+
+	return bson.Raw(doc)
+}
