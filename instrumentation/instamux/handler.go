35ad8575de80c34828b580b7eafaf0573924fca4
Andrey Slotin <andrey.slotin@instana.com>
2021-10-26 14:05:35 +0200

    Use consistent naming in gorilla/mux middleware

--- instrumentation/instamux/handler.go
+++ instrumentation/instamux/handler.go
@@ -15,16 +15,16 @@ import (
 // AddMiddleware instruments the mux.Router instance with Instana
 func AddMiddleware(sensor *instana.Sensor, router *mux.Router) {
 	router.Use(func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			pathTemplate, err := mux.CurrentRoute(r).GetPathTemplate()
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			pathTemplate, err := mux.CurrentRoute(req).GetPathTemplate()
 			if err != nil {
-				sensor.Logger().Debug("can not get path template from the route: ", err.Error())
+				sensor.Logger().Debug("can not get path template from the route: ", err)
 				pathTemplate = ""
 			}
 
-			instana.TracingHandlerFunc(sensor, pathTemplate, func(writer http.ResponseWriter, request *http.Request) {
-				next.ServeHTTP(writer, request)
-			})(w, r)
+			instana.TracingHandlerFunc(sensor, pathTemplate, func(w http.ResponseWriter, req *http.Request) {
+				next.ServeHTTP(w, req)
+			})(w, req)
 		})
 	})
 }
