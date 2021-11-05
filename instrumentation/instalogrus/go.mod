6249cd0e42e7810591cd16d9f3dc58275464b5f5
Andrey Slotin <andrey.slotin@instana.com>
2021-11-05 15:45:09 +0100

    Create a new log span whenever the instalogrus.hook is fired

--- instrumentation/instalogrus/go.mod
+++ instrumentation/instalogrus/go.mod
@@ -4,11 +4,9 @@ go 1.13
 
 require (
 	github.com/instana/go-sensor v1.37.0
-	github.com/sirupsen/logrus v0.4.0
+	github.com/instana/testify v1.6.2-0.20200721153833-94b1851f4d65
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/sirupsen/logrus v1.5.0
 )
 
-require (
-	github.com/looplab/fsm v0.1.0 // indirect
-	github.com/opentracing/opentracing-go v1.1.0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
-)
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
--- instrumentation/instalogrus/instalogrus.go
+++ instrumentation/instalogrus/instalogrus.go
@@ -5,6 +5,7 @@ package instalogrus
 
 import (
 	instana "github.com/instana/go-sensor"
+	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,10 +22,50 @@ func NewHook(sensor *instana.Sensor) *hook {
 
 // Levels returns the list of log levels to be sent to Instana
 func (h *hook) Levels() []logrus.Level {
-	return nil
+	return []logrus.Level{logrus.ErrorLevel, logrus.WarnLevel}
 }
 
 // Fire forwards the logrus.Entry to Instana
 func (h *hook) Fire(entry *logrus.Entry) error {
+	if entry.Context == nil {
+		h.sensor.Logger().Debug("ignoring logrus.Entry without context.Context")
+		return nil
+	}
+
+	parent, ok := instana.SpanFromContext(entry.Context)
+	if !ok {
+		return nil
+	}
+
+	msg, err := entry.String()
+	if err != nil {
+		h.sensor.Logger().Error("failed to obtain logrus.Entry data:", err)
+		return nil
+	}
+
+	h.sensor.Tracer().StartSpan("log.go",
+		opentracing.ChildOf(parent.Context()),
+		opentracing.StartTime(entry.Time),
+		opentracing.Tags{
+			"log.level":   convertLevel(entry.Level),
+			"log.message": string(msg),
+		},
+	).FinishWithOptions(opentracing.FinishOptions{
+		FinishTime: entry.Time,
+	})
+
 	return nil
+}
+
+func convertLevel(lvl logrus.Level) string {
+	switch lvl {
+	case logrus.ErrorLevel:
+		return "ERROR"
+	case logrus.WarnLevel:
+		return "WARN"
+	case logrus.InfoLevel:
+		return "INFO"
+	default:
+		return "DEBUG"
+	}
 }
