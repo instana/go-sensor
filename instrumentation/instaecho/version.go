0a5c22d4092c9936a8655988692c4db48b7b4c7d
Andrey Slotin <andrey.slotin@instana.com>
2021-11-19 11:54:01 +0100

    Add submodule version constants

---
+++ instrumentation/instaecho/version.go
@@ -0,0 +1,7 @@
+// (c) Copyright IBM Corp. 2021
+// (c) Copyright Instana Inc. 2021
+
+package instaecho
+
+// Version is the instrumentation module semantic version
+const Version = "v1.1.1"
---
+++ instrumentation/instalambda/version.go
@@ -0,0 +1,7 @@
+// (c) Copyright IBM Corp. 2021
+// (c) Copyright Instana Inc. 2021
+
+package instalambda
+
+// Version is the instrumentation module semantic version
+const Version = "v1.4.0"
