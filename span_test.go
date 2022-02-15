1a4786d79a2383e386ee088acb97b5ba8f48a02e
Andrii Kushch <>
2022-02-15 17:33:13 +0100

    fix assertion

--- span_test.go
+++ span_test.go
@@ -33,7 +33,7 @@ func TestBasicSpan(t *testing.T) {
 	assert.NotEmpty(t, span.SpanID)
 	assert.NotEmpty(t, span.TraceID)
 	assert.NotEmpty(t, span.Timestamp)
-	assert.LessOrEqual(t, uint64(10*time.Millisecond), span.Duration)
+	assert.LessOrEqual(t, uint64(10), span.Duration)
 	assert.LessOrEqual(t, span.Duration, uint64(elapsed))
 	assert.Equal(t, "sdk", span.Name)
 
