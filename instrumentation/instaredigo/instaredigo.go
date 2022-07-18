5f4dfea374b2391479b56e5fc017e709f718c1ef
sanoj subran <sanoj@sanojs-MacBook-Pro.local>
2022-07-18 07:52:54 +0200

    Added instrumentation support for the redigo/redis library

---
+++ instrumentation/instaredigo/instaredigo.go
@@ -0,0 +1,175 @@
+// (c) Copyright IBM Corp. 2022
+
+//go:build go1.16
+// +build go1.16
+
+package instaredigo
+
+import (
+	"context"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+	instana "github.com/instana/go-sensor"
+	ot "github.com/opentracing/opentracing-go"
+	otlog "github.com/opentracing/opentracing-go/log"
+)
+
+type instaRedigoConn struct {
+	redis.Conn
+	sensor   *instana.Sensor
+	address  string
+	prevSpan *prevSpan
+}
+
+type prevSpan struct {
+	span      ot.Span
+	batchCmds []string
+}
+
+//Dial connects to the Redis server at the given network and address using the specified options along with instrumentation code.
+func Dial(sensor *instana.Sensor, network, address string, options ...redis.DialOption) (redis.Conn, error) {
+	conn, err := redis.Dial(network, address, options...)
+	if strings.HasPrefix(address, ":") {
+		address = "localhost" + address
+	}
+	return &instaRedigoConn{Conn: conn, sensor: sensor, address: address, prevSpan: nil}, err
+}
+
+//DialURL wraps DialURLContext using context.Background along with the instrumentation code.
+func DialURL(sensor *instana.Sensor, rawurl string, options ...redis.DialOption) (redis.Conn, error) {
+	conn, err := redis.DialURL(rawurl, options...)
+	return &instaRedigoConn{conn, sensor, rawurl, nil}, err
+}
+
+//DialURLContext connects to a Redis server at the given URL using the Redis URI scheme along with the instrumentation code.
+func DialURLContext(sensor *instana.Sensor, ctx context.Context, rawurl string, options ...redis.DialOption) (redis.Conn, error) {
+	conn, err := redis.DialURLContext(ctx, rawurl, options...)
+	return &instaRedigoConn{conn, sensor, rawurl, nil}, err
+}
+
+//NewConn returns a new Redigo connection for the given net connection along with the instrumentation code.
+func NewConn(sensor *instana.Sensor, netConn net.Conn, readTimeout, writeTimeout time.Duration) redis.Conn {
+    addr := netConn.LocalAddr().String()
+	conn := redis.NewConn(netConn, readTimeout, writeTimeout)
+	return &instaRedigoConn{conn, sensor, addr, nil}
+}
+
+/// Do sends a command to the server and returns the received reply and collect the instrumentation details. Inorder to capture the correlated span information, create a context from the parent span and pass it as an argument along with the other arguments. The Do API will retrieve the span information from the context and record the correlated span information.
+func (c *instaRedigoConn) Do(commandName string, args ...interface{}) (reply interface{}, err error) {
+	var cmdArgs []interface{}
+	ctx := context.Background()
+    //Separating the passed context and the arguments
+	for _, a := range args {
+		if _, ok := a.(context.Context); ok {
+			ctx = a.(context.Context)
+		} else {
+			cmdArgs = append(cmdArgs, a)
+		}
+	}
+	if c.prevSpan != nil {
+		if strings.ToUpper(commandName) == "EXEC" || strings.ToUpper(commandName) == "DISCARD" {
+			reply, err = c.Conn.Do(commandName, cmdArgs...)
+			if err != nil {
+			    c.prevSpan.span.SetTag("redis.error", err.Error())
+				c.prevSpan.span.LogFields(otlog.Object("error", err.Error()))
+			}
+			c.prevSpan.span.Finish()
+			c.prevSpan = nil
+		} else {
+			c.prevSpan.batchCmds = append(c.prevSpan.batchCmds, commandName)
+			c.prevSpan.span.SetTag("redis.subCommands", c.prevSpan.batchCmds)
+			reply, err = c.Conn.Do(commandName, cmdArgs...)
+			if err != nil {
+			    c.prevSpan.span.SetTag("redis.error", err.Error())
+				c.prevSpan.span.LogFields(otlog.Object("error", err.Error()))
+			}
+		}
+	} else {
+		tracer := c.sensor.Tracer()
+		opts := []ot.StartSpanOption{
+			ot.Tags{
+				"redis.connection": c.address,
+			},
+		}
+		if ps, ok := instana.SpanFromContext(ctx); ok {
+			tracer = ps.Tracer()
+			opts = append(opts, ot.ChildOf(ps.Context()))
+		}
+		span := tracer.StartSpan("redis", opts...)
+		if strings.ToUpper(commandName) == "MULTI" {
+			c.prevSpan = &prevSpan{span, nil}
+		}
+		span.SetTag("redis.command", commandName)
+		reply, err = c.Conn.Do(commandName, cmdArgs...)
+		if err != nil {
+			span.SetTag("redis.error", err.Error())
+			span.LogFields(otlog.Object("error", err.Error()))
+		}
+		if strings.ToUpper(commandName) != "MULTI" {
+			span.Finish()
+		}
+	}
+	return reply, err
+}
+
+// Send writes the command to the client's output buffer and collect the instrumentation details.Inorder to capture the correlated span information, create a context from the parent span and pass it as an argument along with the other arguments. The Send API will retrieve the span information from the context and record the correlated span information.
+func (c *instaRedigoConn) Send(commandName string, args ...interface{}) (err error) {
+	var cmdArgs []interface{}
+    //Separating the parent context from the arguments
+	ctx := context.Background()
+	for _, a := range args {
+		if _, ok := a.(context.Context); ok {
+			ctx = a.(context.Context)
+		} else {
+			cmdArgs = append(cmdArgs, a)
+		}
+	}
+	if c.prevSpan != nil {
+		if strings.ToUpper(commandName) == "EXEC" || strings.ToUpper(commandName) == "DISCARD" {
+			err = c.Conn.Send(commandName, cmdArgs...)
+			if err != nil {
+			    c.prevSpan.span.SetTag("redis.error", err.Error())
+				c.prevSpan.span.LogFields(otlog.Object("error", err.Error()))
+			}
+			c.prevSpan.span.Finish()
+			c.prevSpan = nil
+		} else {
+			c.prevSpan.batchCmds = append(c.prevSpan.batchCmds, commandName)
+			c.prevSpan.span.SetTag("redis.subCommands", c.prevSpan.batchCmds)
+			err = c.Conn.Send(commandName, cmdArgs...)
+			if err != nil {
+			    c.prevSpan.span.SetTag("redis.error", err.Error())
+				c.prevSpan.span.LogFields(otlog.Object("error", err.Error()))
+			}
+		}
+	} else {
+		tracer := c.sensor.Tracer()
+		opts := []ot.StartSpanOption{
+			ot.Tags{
+				"redis.connection": c.address,
+			},
+		}
+		if ps, ok := instana.SpanFromContext(ctx); ok {
+			tracer = ps.Tracer()
+			opts = append(opts, ot.ChildOf(ps.Context()))
+		}
+		span := tracer.StartSpan("redis", opts...)
+		if strings.ToUpper(commandName) == "MULTI" {
+			c.prevSpan = &prevSpan{span, nil}
+		}
+		span.SetTag("redis.command", commandName)
+		err = c.Conn.Send(commandName, cmdArgs...)
+		if err != nil {
+			span.SetTag("redis.error", err.Error())
+			span.LogFields(otlog.Object("error", err.Error()))
+		}
+		if strings.ToUpper(commandName) != "MULTI" {
+			span.Finish()
+		}
+	}
+	return err
+}
+
---
+++ instrumentation/instaredigo/instaredigo_test.go
@@ -0,0 +1,213 @@
+package instaredigo
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/testify/assert"
+	"github.com/instana/testify/require"
+)
+
+type MockConn struct {
+	address string
+}
+
+func (conn *MockConn) Do(commandName string, args ...interface{}) (reply interface{}, err error) {
+	reply = "OK <->" + commandName
+	if len(commandName) == 0 {
+		err = errors.New("Empty command received")
+	}
+	return reply, err
+}
+
+func (conn *MockConn) Send(commandName string, args ...interface{}) error {
+	var err error
+	if len(commandName) == 0 {
+		err = errors.New("Empty command received")
+	}
+	return err
+}
+
+func (conn *MockConn) Receive() (reply interface{}, err error) {
+	reply = "OK"
+	return reply, err
+}
+
+func (conn *MockConn) Err() error {
+	err := errors.New("No error")
+	return err
+}
+
+func (conn *MockConn) Close() error {
+	err := errors.New("No error")
+	return err
+}
+
+func (conn *MockConn) Flush() error {
+	err := errors.New("No error")
+	return err
+}
+
+func TestMockDo(t *testing.T) {
+	examples := map[string]struct {
+		DoCommand []interface{}
+		Expected  instana.RedisSpanTags
+	}{
+		"SET": {
+			DoCommand: []interface{}{"name", "Instana"},
+			Expected: instana.RedisSpanTags{
+				Command: "SET",
+			},
+		},
+		"GET": {
+			DoCommand: []interface{}{"name"},
+			Expected: instana.RedisSpanTags{
+				Command: "GET",
+			},
+		},
+		"DEL": {
+			DoCommand: []interface{}{"name"},
+			Expected: instana.RedisSpanTags{
+				Command: "DEL",
+			},
+		},
+	}
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			recorder := instana.NewTestRecorder()
+			sensor := instana.NewSensorWithTracer(
+				instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+			)
+			sp := sensor.Tracer().StartSpan("testing")
+			defer sp.Finish()
+			conn := &instaRedigoConn{&MockConn{}, sensor, ":7001", nil}
+            defer conn.Close()
+			_, err := conn.Do(name, example.DoCommand...)
+			assert.Equal(t, err, nil)
+			spans := recorder.GetQueuedSpans()
+			assert.Equal(t, 1, len(spans))
+			dbSpan := spans[0]
+			data := dbSpan.Data.(instana.RedisSpanData)
+
+			assert.Equal(t, "redis", dbSpan.Name)
+			assert.EqualValues(t, instana.ExitSpanKind, dbSpan.Kind)
+			assert.Empty(t, dbSpan.Ec)
+
+			require.IsType(t, instana.RedisSpanData{}, dbSpan.Data)
+
+			assert.Equal(t, example.Expected.Error, data.Tags.Error)
+			assert.Equal(t, example.Expected.Command, data.Tags.Command)
+		})
+	}
+}
+
+func TestMockSend(t *testing.T) {
+	examples := map[string]struct {
+		DoCommand []interface{}
+		Expected  instana.RedisSpanTags
+	}{
+		"SET": {
+			DoCommand: []interface{}{"name", "Instana"},
+			Expected: instana.RedisSpanTags{
+				Command: "SET",
+			},
+		},
+		"GET": {
+			DoCommand: []interface{}{"name"},
+			Expected: instana.RedisSpanTags{
+				Command: "GET",
+			},
+		},
+		"DEL": {
+			DoCommand: []interface{}{"name"},
+			Expected: instana.RedisSpanTags{
+				Command: "DEL",
+			},
+		},
+	}
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			recorder := instana.NewTestRecorder()
+			sensor := instana.NewSensorWithTracer(
+				instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+			)
+			sp := sensor.Tracer().StartSpan("testing")
+			defer sp.Finish()
+			conn := &instaRedigoConn{&MockConn{}, sensor, ":7001", nil}
+            defer conn.Close()
+			err := conn.Send(name, example.DoCommand...)
+			assert.Equal(t, err, nil)
+			spans := recorder.GetQueuedSpans()
+			assert.Equal(t, 1, len(spans))
+			dbSpan := spans[0]
+			data := dbSpan.Data.(instana.RedisSpanData)
+
+			assert.Equal(t, "redis", dbSpan.Name)
+			assert.EqualValues(t, instana.ExitSpanKind, dbSpan.Kind)
+			assert.Empty(t, dbSpan.Ec)
+
+			require.IsType(t, instana.RedisSpanData{}, dbSpan.Data)
+
+			assert.Equal(t, example.Expected.Error, data.Tags.Error)
+			assert.Equal(t, example.Expected.Command, data.Tags.Command)
+		})
+	}
+}
+
+
+func TestSubCommands(t *testing.T) {
+	testCases := map[string]struct {
+		BatchCommands [][]interface{}
+		Expected      instana.RedisSpanTags
+	}{
+		"batch commands": {
+			BatchCommands: [][]interface{}{
+				{"multi"},
+				{"set", "name", "IBM"},
+				{"get", "name"},
+				{"del", "name"},
+				{"exec"},
+			},
+			Expected: instana.RedisSpanTags{
+				Command:     "multi",
+				Subcommands: []string{"set", "get", "del"},
+			},
+		},
+	}
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			recorder := instana.NewTestRecorder()
+			sensor := instana.NewSensorWithTracer(
+				instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+			)
+			sp := sensor.Tracer().StartSpan("testing")
+			defer sp.Finish()
+			conn := &instaRedigoConn{&MockConn{}, sensor, ":7001", nil}
+			defer conn.Close()
+			ctx := context.Background()
+			ctxSpan := instana.ContextWithSpan(ctx, sp)
+			for _, cmd := range testCase.BatchCommands {
+				cmdArgs := cmd[1:]
+				cmdArgs = append(cmdArgs, ctxSpan)
+				cmdStr := cmd[0].(string)
+                err := conn.Send(cmdStr, cmdArgs...)
+				assert.Equal(t, nil, err)
+			}
+			spans := recorder.GetQueuedSpans()
+			dbSpan := spans[0]
+			assert.Equal(t, 1, len(spans))
+			data := dbSpan.Data.(instana.RedisSpanData)
+			assert.Equal(t, "redis", dbSpan.Name)
+			assert.EqualValues(t, instana.ExitSpanKind, dbSpan.Kind)
+			assert.Empty(t, dbSpan.Ec)
+
+			require.IsType(t, instana.RedisSpanData{}, dbSpan.Data)
+
+			assert.Equal(t, testCase.Expected.Error, data.Tags.Error)
+			assert.Equal(t, testCase.Expected.Command, data.Tags.Command)
+			assert.Equal(t, testCase.Expected.Subcommands, data.Tags.Subcommands)
+		})
+	}
+}
