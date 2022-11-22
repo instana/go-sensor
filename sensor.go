9ca268f0eb5bbfc340fe8cdd31c9b964e4ca600f
Andrii Kushch <>
2022-11-22 13:41:57 +0100

    revert imports in sensor.go

--- sensor.go
+++ sensor.go
@@ -13,11 +13,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/instana/go-sensor/logger"
-
 	"github.com/instana/go-sensor/acceptor"
 	"github.com/instana/go-sensor/autoprofile"
 	"github.com/instana/go-sensor/aws"
+	"github.com/instana/go-sensor/logger"
 )
 
 const (
