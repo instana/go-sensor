50a90c6939f86ddfd2ca44aca686a569374301e2
Andrii Kushch <>
2022-12-12 15:48:54 +0100

    add cmd

--- bin/sql/sqlgen.go
+++ bin/sql/sqlgen.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+var funcs string
+
 var arrayConn = []string{
 	"driver.Conn",
 	"driver.Execer",
@@ -58,6 +60,7 @@ func main() {
 }
 
 func gen(droverType, prefix string, arr []string) {
+	funcs = ""
 	conn := combinations.All(arr)
 	fconn := [][]string{}
 	for _, v := range conn {
@@ -85,6 +88,8 @@ func gen(droverType, prefix string, arr []string) {
 	genIsAlreadyWrapped(droverType, t)
 
 	genWrapper(prefix, droverType, arr, fconn)
+
+	fmt.Println(funcs)
 }
 
 func genType(prefix string, arr []string, droverType string) string {
@@ -139,40 +144,27 @@ func genWrapper(prefix, t string, originalTypes []string, fconn [][]string) {
 
 	}
 
-	for _, v := range fconn {
-		s := strings.Join(v, " && ")
+	for _, subFconn := range fconn {
+		s := strings.Join(subFconn, " && ")
 		s = strings.ReplaceAll(s, "driver.", "is")
 
 		fmt.Printf("if %s {\n", s)
 
-		name := strings.ReplaceAll(strings.Join(v, "_"), "driver.", "")
+		name := strings.ReplaceAll(strings.Join(subFconn, "_"), "driver.", "")
 
-		fmt.Printf("return &%s {\n", prefix+name)
+		funcs += returnSt(prefix, t, name, subFconn)
 
-		if t == "driver.Stmt" {
-			fmt.Println("Stmt: &wStmt{\nStmt:stmt,\nconnDetails: connDetails,\nquery: query,\nsensor: sensor,\n},")
+		args := ""
+		for _, tt := range subFconn {
+			args += "," + strings.ReplaceAll(tt, "driver.", "")
+		}
+
+		//todo: change
+		if prefix == "w_stmt_" {
+			fmt.Println("return " + "get_stmt_" + name + "(stmt , query , connDetails , sensor  " + args + ")")
 		} else {
-			fmt.Println("Conn: &wConn{\nConn:conn,\nconnDetails: connDetails,\nsensor: sensor,\n},")
+			fmt.Println("return " + "get_conn_" + name + "(connDetails, conn  , sensor  " + args + ")")
 		}
-
-		for _, tt := range v {
-			n := strings.ReplaceAll(tt, "driver.", "")
-
-			if n != "ColumnConverter" && n != "NamedValueChecker" {
-				fmt.Printf("%s: &w%s{\n", n, n)
-				fmt.Printf("%s: %s,\n", n, n)
-				fmt.Println("connDetails: connDetails,")
-				fmt.Println("sensor: sensor,")
-				if t == "driver.Stmt" {
-					fmt.Println("query: query,")
-				}
-				fmt.Println("},")
-			} else {
-				fmt.Printf("%s: %s,\n", n, n)
-			}
-		}
-
-		fmt.Println("}")
 
 		fmt.Println("}")
 	}
@@ -185,4 +177,44 @@ func genWrapper(prefix, t string, originalTypes []string, fconn [][]string) {
 
 	fmt.Println("}")
 
+}
+
+func returnSt(prefix string, t string, name string, v []string) string {
+	res := ""
+
+	args := ""
+	for _, tt := range v {
+		args += "," + strings.ReplaceAll(tt, "driver.", "") + " " + tt
+	}
+
+	if t == "driver.Stmt" {
+		res += "func get_stmt_" + name + "(stmt driver.Stmt, query string, connDetails dbConnDetails, sensor *Sensor " + args + ") driver.Stmt {\n"
+		res += fmt.Sprintf("return &%s {\n", prefix+name)
+		res += fmt.Sprintf("Stmt: &wStmt{\nStmt:stmt,\nconnDetails: connDetails,\nquery: query,\nsensor: sensor,\n},")
+	} else {
+		res += "func get_conn_" + name + "(connDetails dbConnDetails, conn driver.Conn, sensor *Sensor" + args + ") driver.Conn {\n"
+		res += fmt.Sprintf("return &%s {\n", prefix+name)
+		res += fmt.Sprintf("Conn: &wConn{\nConn:conn,\nconnDetails: connDetails,\nsensor: sensor,\n},")
+	}
+
+	for _, tt := range v {
+		n := strings.ReplaceAll(tt, "driver.", "")
+
+		if n != "ColumnConverter" && n != "NamedValueChecker" {
+			res += fmt.Sprintf("%s: &w%s{\n", n, n)
+			res += fmt.Sprintf("%s: %s,\n", n, n)
+			res += fmt.Sprintf("connDetails: connDetails,\n")
+			res += fmt.Sprintf("sensor: sensor,\n")
+			if t == "driver.Stmt" {
+				res += fmt.Sprintf("query: query,\n")
+			}
+			res += fmt.Sprintf("},")
+		} else {
+			res += fmt.Sprintf("%s: %s,\n", n, n)
+		}
+	}
+
+	res += fmt.Sprintf("}\n")
+	res += fmt.Sprintf("}\n")
+	return res
 }
