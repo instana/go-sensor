e6db60e7f304b6b753c305e8c314100f9ab7f1d8
Willian Carvalho <willian.carvalho@instana.com>
2023-01-23 11:28:09 -0300

    added mocked agent always ready

--- instrumentation/instagraphql/instagraphql_test.go
+++ instrumentation/instagraphql/instagraphql_test.go
@@ -12,6 +12,8 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/acceptor"
+	"github.com/instana/go-sensor/autoprofile"
 	"github.com/instana/go-sensor/instrumentation/instagraphql"
 	"github.com/stretchr/testify/require"
 )
@@ -42,7 +44,7 @@ func createField(name string, tp *graphql.Scalar, resolveVal interface{}, err er
 func TestGraphQLBasic(t *testing.T) {
 	recorder := instana.NewTestRecorder()
 	sensor := instana.NewSensorWithTracer(
-		instana.NewTracerWithEverything(instana.DefaultOptions(), recorder),
+		instana.NewTracerWithEverything(&instana.Options{AgentClient: alwaysReadyClient{}}, recorder),
 	)
 
 	qFields := graphql.Fields{
@@ -72,10 +74,10 @@ func TestGraphQLBasic(t *testing.T) {
 	var spans []instana.Span
 
 	assert.Eventually(t, func() bool {
-		spans = recorder.GetQueuedSpans()
-		return len(spans) > 0
-	}, time.Second*10, time.Millisecond*100)
+		return recorder.QueuedSpansCount() == 1
+	}, time.Second*2, time.Millisecond*500)
 
+	spans = recorder.GetQueuedSpans()
 	assert.Len(t, spans, 1)
 
 	require.IsType(t, instana.GraphQLSpanData{}, spans[0].Data)
@@ -89,3 +91,12 @@ func TestGraphQLBasic(t *testing.T) {
 	assert.Equal(t, map[string][]string{"aaa": nil}, data.Tags.Fields)
 	assert.Equal(t, map[string][]string{"aaa": nil}, data.Tags.Args)
 }
+
+type alwaysReadyClient struct{}
+
+func (alwaysReadyClient) Ready() bool                                       { return true }
+func (alwaysReadyClient) SendMetrics(data acceptor.Metrics) error           { return nil }
+func (alwaysReadyClient) SendEvent(event *instana.EventData) error          { return nil }
+func (alwaysReadyClient) SendSpans(spans []instana.Span) error              { return nil }
+func (alwaysReadyClient) SendProfiles(profiles []autoprofile.Profile) error { return nil }
+func (alwaysReadyClient) Flush(context.Context) error                       { return nil }
