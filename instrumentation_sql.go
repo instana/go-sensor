121098ba77907c1b7a26615a60b6b584a5d5b4d3
Willian Carvalho <willian.carvalho@instana.com>
2023-04-18 12:53:34 +0200

    implementation of instagorm

--- instrumentation_sql.go
+++ instrumentation_sql.go
@@ -7,11 +7,11 @@ import (
 	"context"
 	"database/sql"
 	"database/sql/driver"
+	"encoding/json"
 	"net/url"
 	"regexp"
 	"strings"
 	"sync"
-
 	_ "unsafe"
 
 	ot "github.com/opentracing/opentracing-go"
@@ -147,6 +147,31 @@ func parseDBConnDetails(connStr string) dbConnDetails {
 	}
 
 	return dbConnDetails{RawString: connStr}
+}
+
+func GetDBConnectDetails(connStr string) string {
+	strategies := [...]func(string) (dbConnDetails, bool){
+		parseDBConnDetailsURI,
+		parsePostgresConnDetailsKV,
+		parseMySQLConnDetailsKV,
+	}
+
+	details := dbConnDetails{RawString: connStr}
+
+	for _, parseFn := range strategies {
+		if d, ok := parseFn(connStr); ok {
+			details = d
+			break
+		}
+	}
+
+	data, err := json.Marshal(details)
+
+	if err != nil {
+		return ""
+	}
+
+	return string(data)
 }
 
 // parseDBConnDetailsURI attempts to parse a connection string as an URI, assuming that it has
