6fb0467e502a48f8f7e0e0e295a443fafd93f825
Willian Carvalho <willian.carvalho@instana.com>
2023-07-06 08:51:52 +0200

    Limiting grpc instrumentation to build in Go v1.17+
    Due to updates in the version of gRPC to fix vulnerability issues
    we need to limit the version of Go as newer versions of grpc rely
    on io/fs and unsafe.Slice from golang.org/x/sys@v0.4.0

--- instrumentation/instagrpc/instagrpc.go
+++ instrumentation/instagrpc/instagrpc.go
@@ -1,6 +1,9 @@
 // (c) Copyright IBM Corp. 2021
 // (c) Copyright Instana Inc. 2020
 
+//go:build go1.17
+// +build go1.17
+
 // Package instagrpc provides Instana tracing instrumentation for
 // GRPC servers and clients that use google.golang.org/grpc.
 package instagrpc
