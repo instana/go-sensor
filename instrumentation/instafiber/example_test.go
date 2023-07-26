27572602d25b84b286d9a67ac269cd2c22eb6851
sanojsubran <sanoj.subran@ibm.com>
2023-07-26 10:47:52 +0200

    Initial implementation of instafiber

---
+++ instrumentation/instafiber/example_test.go
@@ -0,0 +1,31 @@
+// (c) Copyright IBM Corp. 2023
+
+//go:build go1.17
+
+package instafiber_test
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/instafiber"
+)
+
+func Example() {
+
+	// Create a sensor for instana instrumentation
+	sensor := instana.NewSensor("my-web-server")
+
+	app := fiber.New()
+
+	// Use the instafiber.TraceHandler for instrumenting the handler
+	app.Get("/greet", instafiber.TraceHandler(sensor, "greet", "/greet", hello))
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello world!")
+}
