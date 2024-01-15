bbad45e6b22189c12e5b2f764bcb866524e6abe5
Angith Jayan <angith.j@ibm.com>
2024-01-16 00:28:37 +0530

    added example for cosmos instrumentation

---
+++ example/cosmos/go.mod
@@ -0,0 +1,25 @@
+module cosmos.example
+
+go 1.18.0
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos v0.3.6
+	github.com/google/uuid v1.4.0
+	github.com/instana/go-sensor v1.58.0
+	github.com/instana/go-sensor/instrumentation/instacosmos v1.0.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go v68.0.0+incompatible // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.0 // indirect
+	github.com/looplab/fsm v1.0.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
+
+replace (
+	github.com/instana/go-sensor => ../../
+	github.com/instana/go-sensor/instrumentation/instacosmos v1.0.0 => ../../instrumentation/instacosmos
+)
---
+++ example/cosmos/main.go
@@ -0,0 +1,135 @@
+// (c) Copyright IBM Corp. 2023
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
+	"github.com/google/uuid"
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/instacosmos"
+)
+
+const (
+	database  = "trace-data"
+	container = "spans"
+)
+
+const (
+	CONNECTION_URL = "COSMOS_CONNECTION_URL"
+	KEY            = "COSMOS_KEY"
+)
+
+type SpanType string
+
+const (
+	EntrySpan SpanType = "entry"
+	ExitSpan  SpanType = "exit"
+)
+
+type Span struct {
+	ID          string   `json:"id"`
+	SpanID      string   `json:"SpanID"`
+	Type        SpanType `json:"type"`
+	Description string   `json:"description"`
+}
+
+var (
+	collector instana.TracerLogger
+)
+
+var (
+	endpoint = ""
+	key      = ""
+)
+
+func init() {
+	validateAzureCreds()
+	collector = instana.InitCollector(&instana.Options{
+		Service:           "sample-app-cosmos",
+		EnableAutoProfile: true,
+	})
+}
+
+func main() {
+	http.HandleFunc("/cosmos-test", instana.TracingHandlerFunc(collector, "/cosmos-test", handler))
+	log.Fatal(http.ListenAndServe("localhost:9990", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	// Create a CosmosDB client
+	client, err := getClient(collector)
+	if err != nil {
+		log.Fatal("Failed to create Azure Cosmos DB client: ", err)
+	}
+
+	// Create container client
+	containerClient, err := client.NewContainer(collector, database, container)
+	if err != nil {
+		log.Fatal("Failed to create a container client:", err)
+	}
+
+	id := uuid.New().String()
+	spanID := fmt.Sprintf("span-%s", id)
+
+	// Specifies the value of the partition key
+	partitionKey := fmt.Sprintf("span-%s", spanID)
+	pk := azcosmos.NewPartitionKeyString(partitionKey)
+
+	span := Span{
+		ID:          spanID,
+		SpanID:      partitionKey,
+		Type:        EntrySpan,
+		Description: "sample span",
+	}
+
+	b, err := json.Marshal(span)
+	if err != nil {
+		log.Fatal("Failed to marshal span data:", err)
+	}
+
+	// setting item options upon creating ie. consistency level
+	itemOptions := azcosmos.ItemOptions{
+		ConsistencyLevel: azcosmos.ConsistencyLevelSession.ToPtr(),
+	}
+
+	itemResponse, err := containerClient.CreateItem(context.TODO(), pk, b, &itemOptions)
+	if err != nil {
+		log.Print("Failed to create the item:", err)
+	}
+
+	log.Printf("Status %d. Item %v created. ActivityId %s. Consuming %v Request Units.\n",
+		itemResponse.RawResponse.StatusCode,
+		pk, itemResponse.ActivityID,
+		itemResponse.RequestCharge)
+}
+
+func getClient(sensor instana.TracerLogger) (instacosmos.Client, error) {
+	cred, err := instacosmos.NewKeyCredential(key)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := instacosmos.NewClientWithKey(endpoint, cred, &azcosmos.ClientOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func validateAzureCreds() {
+	endpoint, _ = os.LookupEnv(CONNECTION_URL)
+	key, _ = os.LookupEnv(KEY)
+
+	if endpoint == "" || key == "" {
+		log.Fatal("Azure credentials are not provided")
+	}
+}
