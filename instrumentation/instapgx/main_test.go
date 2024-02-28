6a6a1b801a15cba76f230e9995f5fa206dee32a7
Nithin P <nithin.puthenveettil@ibm.com>
2024-02-28 11:42:00 +0530

    Added runtime-generated database for pgx integration tests
    This feature would enable multiple pgx tests to run in parallel

--- instrumentation/instapgx/main_test.go
+++ instrumentation/instapgx/main_test.go
@@ -10,24 +10,68 @@ import (
 	"database/sql"
 	"log"
 	"math/rand"
+	"os"
+	"strings"
 	"testing"
 
+	"github.com/google/uuid"
 	instana "github.com/instana/go-sensor"
 	"github.com/instana/go-sensor/instrumentation/instapgx"
 	"github.com/jackc/pgx/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-var databaseUrl = "postgres://postgres:mysecretpassword@localhost/postgres"
+var databaseUrl = "postgres://postgres:mysecretpassword@localhost/"
+
+var dbNamePrefix = "db_"
 
 func TestMain(m *testing.M) {
-	db, err := sql.Open("postgres", databaseUrl)
+
+	// Create test db
+	databaseUrlOrg := databaseUrl
+	db, err := sql.Open("postgres", databaseUrl+"?sslmode=disable")
+
 	if err != nil {
 		log.Fatalln("Can not connect to the database:", err.Error())
 	}
+
+	dbName := dbNamePrefix + strings.Replace(uuid.New().String(), "-", "", -1)
+	_, err = db.Exec("CREATE DATABASE " + dbName)
+
+	if err != nil {
+		log.Fatalln("Can not create database: ", err.Error())
+	}
+
+	db.Close()
+
+	// Connect to test db
+	databaseUrl = databaseUrl + dbName
+	db, err = sql.Open("postgres", databaseUrl+"?sslmode=disable")
+
+	if err != nil {
+		log.Fatalln("Can not connect to the database:", err.Error())
+	}
+
+	// Running tests
+	code := m.Run()
+	db.Close()
+
+	// Clean test db
+	db, err = sql.Open("postgres", databaseUrlOrg+"?sslmode=disable")
+
+	if err != nil {
+		log.Fatalln("Can not connect to the database:", err.Error())
+	}
+
 	defer db.Close()
 
-	m.Run()
+	_, err = db.Exec("DROP DATABASE " + dbName + " WITH (FORCE)")
+
+	if err != nil {
+		log.Fatalln("Can not drop database: "+dbName, err.Error())
+	}
+
+	os.Exit(code)
 }
 
 func prepare(t *testing.T) (*instana.Recorder, context.Context, *instapgx.Conn) {
