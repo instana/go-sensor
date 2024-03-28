dd961170220f702d712fa31647972c0f34431175
Nithin P <nithin.puthenveettil@ibm.com>
2024-03-28 11:27:25 +0530

    updated deploy script

--- .tekton/deploy.sh
+++ .tekton/deploy.sh
@@ -6,5 +6,5 @@ kubectl apply --filename secrets.yaml
 kubectl apply --filename task.yaml
 kubectl apply --filename pipeline.yaml
 kubectl apply --filename tekton-triggers-eventlistener-serviceaccount.yaml
-kubectl apply --filename github-pr-eventlistener.yaml
+kubectl apply --filename github-eventlistener.yaml
 kubectl apply --filename github-webhook-ingress.yaml
