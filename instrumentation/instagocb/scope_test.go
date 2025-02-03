03d9591b5777eb25aeb9bd5326a0e0abbe4d42dc
Angith Jayan <angith.j@ibm.com>
2025-02-03 19:55:26 +0530

    refactor: updated unit tests in instagocb with InitCollector API

--- instrumentation/instagocb/scope_test.go
+++ instrumentation/instagocb/scope_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestScope(t *testing.T) {
-	defer instana.ShutdownSensor()
+	defer instana.ShutdownCollector()
 
 	recorder, ctx, cluster, a := prepare(t)
 	defer cluster.Close(&gocb.ClusterCloseOptions{})
