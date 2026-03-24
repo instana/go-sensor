09fee6b822676e363a8a591b426ac78b6e3619bc
Nithin Puthenveettil <nithin.puthenveettil@ibm.com>
2026-03-24 14:01:38 +0530

    feat: instrument go fiber/v3

---
+++ instrumentation/instafiber/v2/example_test.go
@@ -0,0 +1,32 @@
+// (c) Copyright IBM Corp. 2025
+
+package instafiber_test
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/instafiber"
+)
+
+func Example() {
+
+	// Create a collector for instana instrumentation
+	c := instana.InitCollector(&instana.Options{
+		Service: "my-service",
+	})
+	defer instana.ShutdownCollector()
+
+	app := fiber.New()
+
+	// Use the instafiber.TraceHandler for instrumenting the handler
+	app.Get("/greet", instafiber.TraceHandler(c, "greet", "/greet", hello))
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+func hello(c fiber.Ctx) error {
+	return c.SendString("Hello world!")
+}
---
+++ instrumentation/instafiber/v2/version.go
@@ -0,0 +1,6 @@
+// (c) Copyright IBM Corp. 2025
+
+package instafiber
+
+// Version is the instrumentation module semantic version
+const Version = "1.0.0"
